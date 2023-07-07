55d696c5599ee38d861a69d3d2ac15a98552d513
Bob Robey <bobrobey@TheraC10.thera.amd.com>
2023-07-06 20:24:49 -0500

    Updating makefile for portability

--- HIP/vectorAdd/hip_batch_lumi.sh
+++ HIP/vectorAdd/hip_batch_lumi.sh
@@ -18,5 +18,5 @@ cd $HOME/HPCTrainingExamples/HIP/vectorAdd
 
 export HCC_AMDGPU_TARGET=`rocminfo |grep -m 1 -E gfx[^0]{1} | sed -e 's/ *Name: *\(gfx[0-9,a-f]*\) *$/\1/'` 
 
-make vectoradd_hip.exe
-srun ./vectoradd_hip.exe
+make vectoradd
+srun ./vectoradd
