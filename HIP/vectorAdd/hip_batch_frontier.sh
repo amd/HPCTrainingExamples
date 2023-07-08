310ba97141d06c0e234a017cf7f2e3b4a4985d54
Bob Robey <bobrobey@TheraS02.thera.amd.com>
2023-07-08 11:55:07 -0500

    Adding changes for Frontier

---
+++ HIP/vectorAdd/hip_batch_frontier.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -p batch
+#SBATCH -N 1
+#SBATCH --gpus=1
+#SBATCH -t 10:00
+#SBATCH -A <your account id>
+
+module load PrgEnv-amd
+module load amd
+# needed for cmake builds
+module load cmake
+cd $HOME/HPCTrainingExamples/HIP/vectorAdd
+
+# portable Makefile system
+
+make vectoradd
+
+# portable cmake system
+
+#mkdir build && cd build
+#cmake ..
+#make
+
+# Run the vectoradd application
+srun ./vectoradd
