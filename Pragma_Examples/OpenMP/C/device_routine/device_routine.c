14c462e334dc79e64177dd850680c1af46742524
Bob Robey <Bob.Robey@amd.com>
2024-01-18 09:12:44 -0600

    Adding copyright

--- Pragma_Examples/OpenMP/C/device_routine/device_routine.c
+++ Pragma_Examples/OpenMP/C/device_routine/device_routine.c
@@ -1,3 +1,22 @@
+// Copyright (c) 2024 AMD HPC Application Performance Team
+// Author: Bob Robey, Bob.Robey@amd.com
+// MIT License
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+   double ce[2]={0.0, 0.0};
+#pragma target teams distribute parallel do simd reduction(+:ce1[0:2])
+   for (int j = 0; j< 1000; j++){
+      ce[0] += 1.0;
+      ce[1] += 1.0;
+   }
+
+   printf("ce[0] = %lf ce[1] = %lf\n", ce[0], ce[1]);
+   return(0);
+}
+
+
 #include <stdlib.h>
 #include <stdio.h>
 
