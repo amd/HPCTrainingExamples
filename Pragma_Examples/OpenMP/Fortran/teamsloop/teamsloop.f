1f5f8f036173dea0e161d9c6df330de78f88e7e6
Bob Robey <Bob.Robey@amd.com>
2024-02-19 20:06:48 -0600

    Adding teams end loop test

---
+++ Pragma_Examples/OpenMP/Fortran/teamsloop/teamsloop.f
@@ -0,0 +1,11 @@
+program loop
+        integer,dimension(1024) :: mydata
+        integer  :: i
+
+        !$omp target teams loop
+        do i=1,1024
+                mydata(i) = i
+        end do
+        !$omp end target teams loop
+
+end program loop
