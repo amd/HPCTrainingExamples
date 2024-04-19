d26b584288eb0ef12c7e7dccb5bea105cb03d652
Samuel F Antao <samuel.antao@amd.com>
2024-04-19 15:11:17 +0100

    Document changes from original.

--- MLExamples/mnist_DDP_modified.py
+++ MLExamples/mnist_DDP_modified.py
@@ -13,6 +13,12 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
+---
+
+Original code from https://github.com/kubeflow/kubeflow modified to 
+leverage RCCL/NCCL and adjust batch sizes. 
+(Samuel Antao - samuel.antao@amd.com)
 '''
 import datetime
 import logging
