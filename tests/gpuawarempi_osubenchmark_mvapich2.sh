1d0125aeaa66daf5da6cb459b8ab2d0235d5b192
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-07-12 13:48:56 -0500

    Modified test for mvapich2
    It does not run now because the installation is missing
    something, though it runs on Thera after installing
    mvapich2 as in the model installation and placing mpiexec in the PATH

--- tests/gpuawarempi_osubenchmark_mvapich2.sh
+++ tests/gpuawarempi_osubenchmark_mvapich2.sh
@@ -1,43 +1,17 @@
 #!/bin/bash
 
-mkdir OMB && cd OMB
-
-wget https://mvapich.cse.ohio-state.edu/download/mvapich/osu-micro-benchmarks-7.3.tar.gz
-
-tar -xvf osu-micro-benchmarks-7.3.tar.gz
-
-cd osu-micro-benchmarks-7.3
-
 module purge
 
 module load rocm mvapich2/2.3.7 
 
-rm -rf build
-
-mkdir build
-
-./configure --prefix=`pwd`/../build/ \
-	CC=`which mpicc` \
-	CPPFLAGS=-D__HIP_PLATFORM_AMD__=1 \
-        CXX=`which mpicxx` \
-	--enable-rocm \
-	--with-rocm=${ROCM_PATH}
-
-make -j 
-
-make install
-
-ls -l ../build/libexec/osu-micro-benchmarks/mpi
-
 export HIP_VISIBLE_DEVICES=0,1
+
+export MV2_USE_ROCM=1
 
 # NOTE: since mvapich2 needs a hostfile to run on multiple nodes
 # this benchmark is run only on the local host unlike the openmpi
 # analog (i.e. gpuawarempi_osubenchmark_openmpi.sh) where we specify
 # -N 2 in the run command
 
-mpiexec -np 2  ../build/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bw -m 10240000
+mpiexec -np 2 $MV2_PATH/libexec/osu-microbenchmarks/mpi/collective/osu_allreduce -d rocm
 
-cd ../..
-
-rm -rf OMB
