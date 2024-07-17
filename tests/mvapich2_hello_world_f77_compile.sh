33f11755e3594434e1bdf44b3bed43511aa483a3
Bob Robey <Bob.Robey@amd.com>
2024-07-17 13:39:13 -0500

    Fixes to Fortran MPI hello world

--- tests/mvapich2_hello_world_f77_compile.sh
+++ tests/mvapich2_hello_world_f77_compile.sh
@@ -6,25 +6,26 @@ mkdir mvapich2_hello_world_f77_compile && cd mvapich2_hello_world_f77_compile
 
 cat <<-EOF > mpi_hello_world.f77
 	program mpi_hello_world
-        include 'mpi.h'
+	include 'mpif.h'
 
 	integer ierror, size, rank
 
-        // Initialize the MPI environment
-        call MPI_Init(ierror);
+	! Initialize the MPI environment
+	call MPI_Init(ierror);
 
-        // Get the number of processes
-        call MPI_Comm_size(MPI_COMM_WORLD, size, ierror);
+	! Get the number of processes
+	call MPI_Comm_size(MPI_COMM_WORLD, size, ierror);
 
-        // Get the rank of the process
-        call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierror);
+	! Get the rank of the process
+	call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierror);
 
-        // Print off a hello world message
-        print *,"Hello world from rank ",rank, " out of ", size, " processors"
+	! Print off a hello world message
+	print *,"Hello world from rank ", rank, " out of ", size, " processors"
 
-        // Finalize the MPI environment.
-        call MPI_Finalize();
-        }
+	! Finalize the MPI environment.
+	call MPI_Finalize();
+
+	end program
 EOF
 
 mpif77 -o mpi_hello_world mpi_hello_world.f77
