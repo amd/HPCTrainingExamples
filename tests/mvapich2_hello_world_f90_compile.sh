7edca85ac5f2c053e2280bcef294b86516c81d1f
Bob Robey <Bob.Robey@amd.com>
2024-07-17 13:06:37 -0500

    Adding more mvapich compile tests

---
+++ tests/mvapich2_hello_world_f90_compile.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+module load mvapich2
+
+mkdir mvapich2_hello_world_f90_compile && cd mvapich2_hello_world_f90_compile
+
+cat <<-EOF > mpi_hello_world.f90
+	program mpi_hello_world
+        include 'mpi.h'
+
+	integer ierror, size, rank
+
+        // Initialize the MPI environment
+        call MPI_Init(ierror);
+
+        // Get the number of processes
+        call MPI_Comm_size(MPI_COMM_WORLD, size, ierror);
+
+        // Get the rank of the process
+        call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierror);
+
+        // Print off a hello world message
+        print *,"Hello world from rank ",rank, " out of ", size, " processors"
+
+        // Finalize the MPI environment.
+        call MPI_Finalize();
+        }
+EOF
+
+mpif90 -o mpi_hello_world mpi_hello_world.f90
+if [ -x mpi_hello_world ]; then
+  echo "Executable created"
+fi
+
+cd ..
+rm -rf mvapich2_hello_world_f90_compile
