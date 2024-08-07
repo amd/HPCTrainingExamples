a02f442152801c037722873c6cb3833576f5ead0
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-08-07 15:01:07 -0500

    Modified omnitrace avail test script

--- tests/omnitrace-avail_check.sh
+++ tests/omnitrace-avail_check.sh
@@ -3,14 +3,16 @@
 module purge
 
 module load rocm
-ROCM_VERSION=`cat ${ROCM_PATH}.info/version | head -1 | cut -f1 -d'-' `
+ROCM_VERSION=`cat ${ROCM_PATH}/.info/version | head -1 | cut -f1 -d'-' `
 result=`echo ${ROCM_VERSION} | awk '$1<=6.1.2'` && echo $result
 module unload rocm
 
 if [[ "${result}" ]]; then
    module load omnitrace
+   echo "loaded omnitrace from AMD Research"
 else
    module load rocm
+   echo "loaded omnitrace from ROCm"
 fi   
 
 omnitrace-avail -G $PWD/.omnitrace.cfg
