7ecbb522b976607a58e0bf6a1072481fb3dbf95c
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-08-08 15:26:07 -0500

    Added omniperf_analyze_check.sh

---
+++ tests/omniperf_analyze_check.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# This test checks that 
+# omniperf profile runs
+
+module purge
+
+module load rocm
+REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+ROCM_VERSION=`cat ${ROCM_PATH}/.info/version | head -1 | cut -f1 -d'-' `
+pushd ${REPO_DIR}/MPI-examples/GhostExchange/GhostExchange_ArrayAssign_HIP/Ver1
+mkdir build_for_test
+cd build_for_test
+cmake -DCMAKE_CXX_COMPILER=/opt/rocm-${ROCM_VERSION}/bin/amdclang++ -DCMAKE_C_COMPILER=/opt/rocm-${ROCM_VERSION}/bin/amdclang ..
+make -j
+
+result=`echo ${ROCM_VERSION} | awk '$1<=6.1.2'` && echo $result
+module unload rocm
+
+if [[ "${result}" ]]; then
+   module load omniperf
+   echo "loaded omniperf from AMD Research"
+   echo " "
+else
+   module load rocm
+   echo "loaded omniperf from ROCm"
+   echo " "
+fi   
+
+export HSA_XNACK=1
+omniperf profile -n v1 --no-roof -- ./GhostExchange -x 1 -y 1 -i 200 -j 200 -h 2 -t -c -I 100
+sed -i "/dim3 block2(64, 4, 1);/c\dim3 block2(128, 4, 1);" ../GhostExchange.hip
+omniperf profile -n v2 --no-roof -- ./GhostExchange -x 1 -y 1 -i 200 -j 200 -h 2 -t -c -I 100
+omniperf analyze –p workloads/v1/* –p workloads/v2/* --dispatch 1 --block 7.1.0 7.1.1 7.1.2 7.1.0: Grid size 7.1.1: Workgroup size 7.1.2: Total Wavefronts
+
+cd ..
+rm -rf build_for_test
+popd
