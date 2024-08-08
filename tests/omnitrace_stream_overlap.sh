4149a03b69f2e922f520672c02df4b5cbb0963ba
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-08-08 10:25:38 -0500

    Added omnitrace_stream_overlap.sh

---
+++ tests/omnitrace_stream_overlap.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+module purge
+
+module load rocm
+REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+pushd ${REPO_DIR}/HIP/Stream_Overlap/0-Orig/
+mkdir build_for_test; cd build_for_test
+cmake ../
+make -j
+
+ROCM_VERSION=`cat ${ROCM_PATH}/.info/version | head -1 | cut -f1 -d'-' `
+result=`echo ${ROCM_VERSION} | awk '$1<=6.1.2'` && echo $result
+module unload rocm
+
+if [[ "${result}" ]]; then
+   module load omnitrace
+   echo "loaded omnitrace from AMD Research"
+   echo " "
+else
+   module load rocm
+   echo "loaded omnitrace from ROCm"
+   echo " "
+fi  
+
+omnitrace-avail -G $PWD/.omnitrace.cfg
+export OMNITRACE_CONFIG_FILE=$PWD/.omnitrace.cfg
+omnitrace-instrument -o compute_comm_overlap.inst -- compute_comm_overlap
+omnitrace-run -- ./compute_comm_overlap.inst 2
+cd omnitrace-compute_comm_overlap.inst-output/
+ls *
+
+cd ..
+rm -rf build_for_test
+
+popd
+
