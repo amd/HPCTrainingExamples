a9c14f10ae6804f195d53e952661c7fa86f422b6
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-08-09 15:00:37 -0500

    Added omniperf version input to omniperf test suite tests

--- tests/omniperf_profile_check.sh
+++ tests/omniperf_profile_check.sh
@@ -3,6 +3,52 @@
 # This test checks that 
 # omniperf profile runs
 
+OMNIPERF_VERSION=""
+
+usage()
+{
+    echo ""
+    echo "--help : prints this message"
+    echo "--omniperf-version : specifies the omniperf version"
+    echo ""
+    exit
+}
+
+send-error()
+{
+    usage
+    echo -e "\nError: ${@}"
+    exit 1
+}
+
+reset-last()
+{
+   last() { send-error "Unsupported argument :: ${1}"; }
+}
+
+n=0
+while [[ $# -gt 0 ]]
+do
+   case "${1}" in
+      "--omniperf-version")
+          shift
+          OMNIPERF_VERSION=${1}
+          reset-last
+          ;;
+     "--help")
+          usage
+          ;;
+      "--*")
+          send-error "Unsupported argument at position $((${n} + 1)) :: ${1}"
+          ;;
+      *)
+         last ${1}
+         ;;
+   esac
+   n=$((${n} + 1))
+   shift
+done
+
 module purge
 
 module load rocm
@@ -18,14 +64,32 @@ result=`echo ${ROCM_VERSION} | awk '$1<=6.1.2'` && echo $result
 module unload rocm
 
 if [[ "${result}" ]]; then
-   module load omniperf
+   echo " ------------------------------- "
+   echo " "
    echo "loaded omniperf from AMD Research"
    echo " "
+   echo " ------------------------------- "
+   echo " "
+   echo "module load omniperf${OMNIPERF_VERSION}"
+   echo " "
+   echo " ------------------------------- "
+   module show omniperf${OMNIPERF_VERSION}
+   module load omniperf${OMNIPERF_VERSION}
 else
-   module load rocm
+   echo " ------------------------------- "
+   echo " "
    echo "loaded omniperf from ROCm"
    echo " "
-fi   
+   echo " ------------------------------- "
+   echo " "
+   echo "module load omniperf${OMNIPERF_VERSION}"
+   echo " "
+   echo " ------------------------------- "
+   module show omniperf${OMNIPERF_VERSION}
+   module load rocm
+   module load omniperf${OMNIPERF_VERSION}
+   echo " "
+fi
 
 export HSA_XNACK=1
 omniperf profile -n v1 --no-roof -- ./GhostExchange -x 1 -y 1 -i 200 -j 200 -h 2 -t -c -I 100
