078da7c06824d8220a6e2dcd5c5c2ff45897b423
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-08-13 15:32:05 -0500

    Path of meson changed in test

--- tests/hpctoolkit_unit_tests.sh
+++ tests/hpctoolkit_unit_tests.sh
@@ -6,9 +6,7 @@ module load hpctoolkit
 
 pip install pipx
 pipx install 'meson>=1.3.2'
-# the two lines below are to make sure meson is in the PATH
-pipx ensurepath
-#export PATH=$HOME/.local/bin:$PATH
+export PATH=$HOME/.local/bin:$PATH
 
 cd ${HPCTOOLKIT_PATH}
 meson test
