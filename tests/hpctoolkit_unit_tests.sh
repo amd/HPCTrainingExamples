c3446fe5bf6cbe7bf26722644b30e3c0a61026bd
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-08-13 15:04:35 -0500

    Small change

--- tests/hpctoolkit_unit_tests.sh
+++ tests/hpctoolkit_unit_tests.sh
@@ -8,7 +8,7 @@ pip install pipx
 pipx install 'meson>=1.3.2'
 # the two lines below are to make sure meson is in the PATH
 pipx ensurepath
-export PATH=$HOME/.local/bin:$PATH
+#export PATH=$HOME/.local/bin:$PATH
 
 cd ${HPCTOOLKIT_PATH}
 meson test
