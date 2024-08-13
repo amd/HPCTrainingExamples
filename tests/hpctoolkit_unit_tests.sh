7259b03822d5f477bee422f4b2c78af0ac277715
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-08-13 15:03:43 -0500

    Modified hpctoolkit_unit_tests.sh

--- tests/hpctoolkit_unit_tests.sh
+++ tests/hpctoolkit_unit_tests.sh
@@ -4,8 +4,18 @@ module purge
 module load rocm
 module load hpctoolkit
 
+pip install pipx
+pipx install 'meson>=1.3.2'
+# the two lines below are to make sure meson is in the PATH
+pipx ensurepath
+export PATH=$HOME/.local/bin:$PATH
+
 cd ${HPCTOOLKIT_PATH}
 meson test
+
+pipx uninstall meson
+pip uninstall -y pipx
+
 
 
 
