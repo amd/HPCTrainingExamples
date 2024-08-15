9b6530737a39ae19018f4955b60706329482858a
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-08-15 01:35:35 -0500

    Added module name input argument for mpi4py tests

--- tests/mpi4py_check_import_mpi.sh
+++ tests/mpi4py_check_import_mpi.sh
@@ -7,6 +7,52 @@
 # to the instructions available in the model installation repo:
 # https://github.com/amd/HPCTrainingDock/blob/main/rocm/sources/scripts/openmpi_setup.sh
 
+MODULE_NAME=""
+
+usage()
+{
+    echo ""
+    echo "--help : prints this message"
+    echo "--module-name : specifies the module to load to get mpi4py"
+    echo ""
+    exit
+}
+
+send-error()
+{
+    usage
+    echo -e "\nError: ${@}"
+    exit 1
+}
+
+reset-last()
+{
+   last() { send-error "Unsupported argument :: ${1}"; }
+}
+
+n=0
+while [[ $# -gt 0 ]]
+do
+   case "${1}" in
+      "--module-name")
+          shift
+          MODULE_NAME=${1}
+          reset-last
+          ;;
+     "--help")
+          usage
+          ;;
+      "--*")
+          send-error "Unsupported argument at position $((${n} + 1)) :: ${1}"
+          ;;
+      *)
+         last ${1}
+         ;;
+   esac
+   n=$((${n} + 1))
+   shift
+done
+
 module purge
 
 module load openmpi
