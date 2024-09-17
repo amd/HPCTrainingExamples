5633f0deb232cfea475b104085b53c0292a23445
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-09-16 21:01:09 -0500

    Revised TAU tests

--- tests/tau_check_hip_activity.sh
+++ tests/tau_check_hip_activity.sh
@@ -4,12 +4,11 @@ REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 cd ${REPO_DIR}/HIP/jacobi
 
 module purge
-module load rocm
 module load tau
 
 make
 
-tau_exec -T rocm -ebs   ./Jacobi_hip -g 1 1
+mpirun -n 2 tau_exec -T rocm -ebs   ./Jacobi_hip -g 2 1
 pprof
 
 
