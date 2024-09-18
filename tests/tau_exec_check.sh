a0b3436f1f6b78b4862e0fbfcc24edc56b2d5b33
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-09-18 00:35:45 -0500

    Modified TAU tests

---
+++ tests/tau_exec_check.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+TAU_TRACE=0
+TAU_PROFILE=0
+
+usage()
+{
+    echo ""
+    echo "--help : prints this message"
+    echo "--tau-trace : sets TAU_TRACE=1 - default is TAU_TRACE=0"
+    echo "--tau-profile : sets TAU_PROFILE=1 - default is TAU_PROFILE=0"
+    echo ""
+    exit
+}
+
+
+send-error()
+{
+    usage
+    echo -e "\nError: ${@}"
+    exit 1
+}
+
+reset-last()
+{
+   last() { send-error "Unsupported argument :: ${1}"; }
+}
+
+n=0
+while [[ $# -gt 0 ]]
+do
+   case "${1}" in
+      "--tau-trace")
+          shift
+          TAU_TRACE=1
+          reset-last
+          ;;
+      "--tau-profile")
+          shift
+          TAU_PROFILE=1
+          reset-last
+          ;;
+     "--help")
+          usage
+          ;;
+      "--*")
+          send-error "Unsupported argument at position $((${n} + 1)) :: ${1}"
+          ;;
+      *)
+         last ${1}
+         ;;
+   esac
+   n=$((${n} + 1))
+   shift
+done
+
+
+REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+cd ${REPO_DIR}/HIP/jacobi
+
+module purge
+module load tau
+
+export TAU_PROFILE=${TAU_PROFILE}
+export TAU_TRACE=${TAU_TRACE}
+
+make
+
+mpirun -n 2 tau_exec -T rocm -ebs   ./Jacobi_hip -g 2 1
+pprof
+
+
