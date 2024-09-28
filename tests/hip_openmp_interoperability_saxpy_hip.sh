b38d1cf98c95463b0949da4cfd7801ad48efbea3
Bob Robey <Bob.Robey@amd.com>
2024-09-27 21:52:05 -0500

    Adding openmp hip interoperability tests

---
+++ tests/hip_openmp_interoperability_saxpy_hip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+module load rocm
+XNACK_COUNT=`rocminfo | grep xnack | wc -l`
+if [ ${XNACK_COUNT} -lt 1 ]; then
+   echo "Skip"
+else
+   export HSA_XNACK=1
+   module load amdclang
+
+   REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+   cd ${REPO_DIR}/HIP-OpenMP/CXX/saxpy_openmp_hip
+   make
+
+   make clean
+fi   
