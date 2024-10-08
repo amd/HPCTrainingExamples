a9862da6909c06bceec9b510528d4a96931582ee
Bob Robey <Bob.Robey@amd.com>
2024-10-08 12:54:30 -0500

    fixing more openmp tests

--- tests/openmp_freduce_f_flang.sh
+++ tests/openmp_freduce_f_flang.sh
@@ -3,7 +3,7 @@
 module load clang/15
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/freduce
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/2_freduce
 
 make
 ./freduce
--- tests/openmp_vecadd_f_amdflang.sh
+++ tests/openmp_vecadd_f_amdflang.sh
@@ -3,7 +3,7 @@
 module load amdclang
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/vecadd
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/3_vecadd
 
 make
 ./vecadd
