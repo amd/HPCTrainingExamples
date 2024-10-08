64767c841eac29432617bc3db544af8760bbf18a
Bob Robey <Bob.Robey@amd.com>
2024-10-08 13:51:28 -0500

    Fixes for USM examples

--- tests/openmp_language_constructs_fortran_dtype_pointer.sh
+++ tests/openmp_language_constructs_fortran_dtype_pointer.sh
@@ -2,7 +2,7 @@
 
 module load amdclang
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/6_derived_types
 cd derived_types
 make dtype_pointer
 ./dtype_pointer
--- tests/openmp_language_constructs_fortran_reduction_scalar.sh
+++ tests/openmp_language_constructs_fortran_reduction_scalar.sh
@@ -2,7 +2,7 @@
 
 module load amdclang
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/4_reduction_scalar/1_reduction_scalar_solution
 cd reduction_scalar
 make
 ./reduction_scalar
