92dafeb86b615838d425bdd4fcd19b82431fc2f3
Bob Robey <Bob.Robey@amd.com>
2024-10-08 13:09:09 -0500

    more fixes to openmp examples

--- tests/openmp_freduce_f_gfortran.sh
+++ tests/openmp_freduce_f_gfortran.sh
@@ -3,7 +3,7 @@
 module load gcc/13
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/2_freduce
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/2_freduce/1_reduction_solution
 
 make
 ./freduce
