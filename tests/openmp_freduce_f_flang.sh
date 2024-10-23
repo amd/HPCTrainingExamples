e7baf88d6288ebf5644dc513a843e878f50a9259
Johanna Potyka <johanna.potyka@amd.com>
2024-10-23 19:07:52 +0000

    fix wrong paths in tests

--- tests/openmp_freduce_f_flang.sh
+++ tests/openmp_freduce_f_flang.sh
@@ -3,7 +3,7 @@
 module load clang/15
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/2_freduce
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/2_reduction/1_reduction_solution
 
 make
 ./freduce
