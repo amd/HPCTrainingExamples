fdba5da760f1c6c4b76da8f4d19b4e4c8af6220f
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-12-04 17:34:52 -0600

    Minor edit to netcdf-fortra test

--- tests/netcdf-fortran_simple_xy_wr.sh
+++ tests/netcdf-fortran_simple_xy_wr.sh
@@ -10,7 +10,7 @@ rm -rf netcdf-fortran_test
 mkdir netcdf-fortran_test
 cd netcdf-fortran_test
 wget https://www.unidata.ucar.edu/software/netcdf/examples/programs/simple_xy_wr.f 
-$FC simple_xy_wr.f -I${NETCDF_FC_ROOT}/include -L${NETCDF_FC_ROOT}/lib -lnetcdff -L${NETCDF_C_ROOT} -lnetcdf -o simple_xy_wr
+$FC simple_xy_wr.f -I${NETCDF_FC_ROOT}/include -L${NETCDF_FC_ROOT}/lib -lnetcdff -o simple_xy_wr
 ./simple_xy_wr
 rm -rf netcdf-fortran_test
 
