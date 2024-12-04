fc84d420a6a7a7163632c446eb45c587d6a4cc95
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-12-04 17:31:43 -0600

    Added netcdf-fortran test

--- tests/netcdf-c_simple_xy_wr.sh
+++ tests/netcdf-c_simple_xy_wr.sh
@@ -2,7 +2,6 @@
 
 module purge
 module load netcdf-c
-module load rocm
 
 # use the compiler used to build netcdf-c
 CC=`nc-config --cc`
@@ -10,8 +9,8 @@ CC=`nc-config --cc`
 rm -rf netcdf-c_test
 mkdir netcdf-c_test
 cd netcdf-c_test
-wget https://people.sc.fsu.edu/~jburkardt/c_src/netcdf_test/simple_xy_wr.c
-$CC simple_xy_wr.c -lnetcdf -L/opt/rocmplus-6.2.4/netcdf/netcdf-c/lib -o simple_xy_wr
+wget https://www.unidata.ucar.edu/software/netcdf/examples/programs/simple_xy_wr.c
+$CC simple_xy_wr.c -lnetcdf -L${NETCDF_C_ROOT}/lib -o simple_xy_wr
 ./simple_xy_wr
 rm -rf netcdf-c_test
 
---
+++ tests/netcdf-fortran_simple_xy_wr.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+module purge
+module load netcdf-fortran
+
+# use the compiler used to build netcdf-c
+FC=`nc-config --fc`
+
+rm -rf netcdf-fortran_test
+mkdir netcdf-fortran_test
+cd netcdf-fortran_test
+wget https://www.unidata.ucar.edu/software/netcdf/examples/programs/simple_xy_wr.f 
+$FC simple_xy_wr.f -I${NETCDF_FC_ROOT}/include -L${NETCDF_FC_ROOT}/lib -lnetcdff -L${NETCDF_C_ROOT} -lnetcdf -o simple_xy_wr
+./simple_xy_wr
+rm -rf netcdf-fortran_test
+
+
+
