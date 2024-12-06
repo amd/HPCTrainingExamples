21617fb2890803fb4f1abcd229ce8bbce231d8bf
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-12-06 12:02:03 -0600

    Added parallel I/O test for netcdf-fortran

---
+++ tests/netcdf-fortran_simple_xy_par.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+module purge
+module load netcdf-c
+module load netcdf-fortran
+
+# use the compiler used to build netcdf-fortran
+FC=`nf-config --fc`
+
+rm -rf netcdf-fortran_test
+mkdir netcdf-fortran_test
+cd netcdf-fortran_test
+git clone https://github.com/Unidata/netcdf-fortran.git
+$FC  ./netcdf-fortran/examples/F90/simple_xy_par_wr.F90 -o simple_xy_par_wf  -I${NETCDF_FC_ROOT}/include -L${NETCDF_FC_ROOT}/lib -lnetcdff -L${PNETCDF_ROOT}/lib -lpnetcdf
+mpirun -n 4 ./simple_xy_par_wf
+ncdump simple_xy_par.nc
+cd ..
+rm -rf netcdf-fortran_test
+
+
+
