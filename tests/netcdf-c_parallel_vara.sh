788856b5490e4daaa00e809a7552881fc0dd7b46
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-12-06 10:54:17 -0600

    Added new test to check parallel I/O for netcdf

---
+++ tests/netcdf-c_parallel_vara.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+module purge
+module load netcdf-c
+
+# use the compiler used to build netcdf-c
+CC=`nc-config --cc`
+
+rm -rf netcdf-c_test
+mkdir netcdf-c_test
+cd netcdf-c_test
+git clone https://github.com/Unidata/netcdf-c.git
+$CC -O2 ./netcdf-c/examples/C/parallel_vara.c -o parallel_vara -L${NETCDF_C_ROOT}/lib -lnetcdf -L${PNETCDF_ROOT}/lib -lpnetcdf
+mpirun -n 4 ./parallel_vara testfile.nc
+ncdump testfile.nc
+cd ..
+rm -rf netcdf-c_test
+
+
+
---
+++ tests/netcdf-c_pres_temp_4D.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+module purge
+module load netcdf-c
+
+# use the compiler used to build netcdf-c
+CC=`nc-config --cc`
+
+rm -rf netcdf-c_test
+mkdir netcdf-c_test
+cd netcdf-c_test
+git clone https://github.com/Unidata/netcdf-c.git
+$CC ./netcdf-c/examples/C/pres_temp_4D_wr.c -lnetcdf -L${NETCDF_C_ROOT}/lib -o pres_temp_4D_wr
+$CC ./netcdf-c/examples/C/pres_temp_4D_rd.c -lnetcdf -L${NETCDF_C_ROOT}/lib -o pres_temp_4D_rd
+./pres_temp_4D_wr
+./pres_temp_4D_rd
+cd ..
+rm -rf netcdf-c_test
+
+
+
