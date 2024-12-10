b62f0a1736b1640422eeb703bbb664307ed2ae1b
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-12-09 18:05:43 -0600

    Fixed hipifly_with_module test

--- tests/hipifly_with_module.sh
+++ tests/hipifly_with_module.sh
@@ -5,12 +5,13 @@ module load hipifly
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 
-mkdir hipifly_test && hipifly_test
-cp -a ${REPO_DIR}/hipifly/.
+mkdir hipifly_test && cd hipifly_test
+cp -a ${REPO_DIR}/hipifly/ .
 pushd hipifly/vector_add
 rm src/hipifly.h
 
-make DFLAGS="-DENABLE_HIP -${HIPIFLY_PATH} -fPIE"
+make DFLAGS="-DENABLE_HIP -I${HIPIFLY_PATH} -fPIE"
 ./vector_add
 popd
 rm -rf hipifly_test
+
