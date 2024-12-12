b21494ad7e6b698d07d14edeb6163bc413684e3a
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2024-12-12 10:25:03 -0600

    improving netcdf sanity checks

--- tests/netcdf-c_print_config.sh
+++ tests/netcdf-c_print_config.sh
@@ -3,5 +3,10 @@
 module purge
 module load netcdf-c
 
+if [[ ${HDF5_ENABLE_PARALLEL} == "OFF" ]]; then
+   # NETCDF has not been built with parallel I/O support
+   echo "Skip"
+fi
+
 nc-config --all
 
