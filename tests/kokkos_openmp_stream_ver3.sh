a3d858320ba7586c45a9a112d2d939a055299896
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2025-01-22 19:21:03 -0600

    Added module load kokkos to some tests

--- tests/kokkos_openmp_stream_ver3.sh
+++ tests/kokkos_openmp_stream_ver3.sh
@@ -26,7 +26,7 @@ rm -rf Kokkos_build
 export Kokkos_DIR=${PWDir}/Kokkos_OpenMP
 
 git clone --recursive https://github.com/EssentialsOfParallelComputing/Chapter13 Chapter13
-cd Chapter13/Kokkos/StreamTriad/Ver2
+cd Chapter13/Kokkos/StreamTriad/Ver3
 sed -i -e 's/80000000/100000/' StreamTriad.cc
 
 export OMP_PROC_BIND=spread
