05d8d6ed27d4f68f0d52d583b90dac77aafd7781
Bob Robey <Bob.Robey@amd.com>
2025-02-18 16:27:04 -0600

    Adding openmp atomic example

---
+++ Pragma_Examples/OpenMP/C/Atomics/atomic.c
@@ -0,0 +1,39 @@
+/*
+Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved.
+*/
+
+#include <stdio.h>
+#include <omp.h>
+#include <math.h>
+
+#pragma omp requires unified_shared_memory
+
+int main()
+{
+  int N = 100000;
+  double sum;
+
+  // Input arrays
+  double a[N];
+
+  #pragma omp target teams distribute parallel for
+  for (int i=0; i<N; i++){
+    a[i]=1.0;
+  }
+  double tstart = omp_get_wtime();
+  sum = 0.0;
+  #pragma omp target teams distribute parallel for map(tofrom:sum)
+  for (int i = 0; i< N; i++){
+    #pragma omp atomic
+    sum += a[i];
+  }
+  printf("   Atomic result: %lf Runtime is: %lf secs\n", sum, omp_get_wtime()-tstart);
+
+  tstart = omp_get_wtime();
+  sum = 0.0;
+  #pragma omp target teams distribute parallel for reduction(+: sum)
+  for (int i=0; i<N; i++){
+    sum += a[i];
+  }
+  printf("Reduction result: %lf Runtime is: %lf secs\n", sum, omp_get_wtime()-tstart);
+}
