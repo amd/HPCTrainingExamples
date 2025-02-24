552f261a198826e2d083e5c643ac797cd9b1c6fc
Bob Robey <Bob.Robey@amd.com>
2025-02-24 16:13:46 -0600

    Fixing test paths

--- tests/openmp_language_constructs_cxx_member_function_external.sh
+++ tests/openmp_language_constructs_cxx_member_function_external.sh
@@ -2,7 +2,7 @@
 
 module load amdclang
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-cd ${REPO_DIR}/Pragma_Examples/OpenMP/CXX/5_device_routines/2_member_function_external/1_member_function
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/CXX/5_device_routines/2_member_function_external/1_member_function_external
 make
 ./bigscience
 
