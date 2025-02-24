546a5432ac556047cfea30fa83ae864f61d46e48
Bob Robey <Bob.Robey@amd.com>
2025-02-24 17:02:13 -0600

    Cleaning up paths

--- tests/openmp_language_constructs_cxx_member_function_data_external.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-module load amdclang
-REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-cd ${REPO_DIR}/Pragma_Examples/OpenMP/CXX/5_device_routines/2_member_function_external/2_member_function_data
-make
-./bigscience
-
-make clean
