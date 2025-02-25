865468a13318af57e6a9f8a0eb815e3e9ad68614
Bob Robey <Bob.Robey@amd.com>
2025-02-24 18:47:36 -0600

    Adding virtual methods

---
+++ Pragma_Examples/OpenMP/CXX/5_device_routines/3_virtual_methods/0_virtual_methods_portyourself/HotScience_member_functions.cc
@@ -0,0 +1,5 @@
+#include "HotScience.hh"
+
+void HotScience::compute(double *x, int N){
+   *x = 5.0;
+}
---
+++ Pragma_Examples/OpenMP/CXX/5_device_routines/3_virtual_methods/1_virtual_methods/bigscience.cc
@@ -0,0 +1,29 @@
+// Copyright (c) 2025 AMD HPC Application Performance Team
+// Author: Bob Robey, Bob.Robey@amd.com
+// MIT License
+
+#include <iostream>
+
+#include "HotScience.hh"
+
+using namespace std;
+
+//#pragma omp requires unified_shared_memory
+
+int main(int argc, char *argv[]){
+
+   HotScience myscienceclass;
+
+   int N=10000;
+   double *x = new double[N];
+
+#pragma omp target teams loop
+   for (int k = 0; k < N; k++){
+      myscienceclass.compute(&x[k], N);
+   }
+
+   delete[] x;
+
+   cout << "Array value is " << x[0] << endl;
+   cout << "Finished calculation" << endl;
+}
