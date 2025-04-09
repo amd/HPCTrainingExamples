6387da15e668da01fd636c976cb459dd06adbc11
Bob Robey <Bob.Robey@amd.com>
2025-04-09 07:53:29 -0500

    Fixing OpenMP Language Constructs that are failing due to missing HSA_XNACK=1

--- tests/openmp_language_constructs_cxx_member_function_external.sh
+++ tests/openmp_language_constructs_cxx_member_function_external.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 module load amdclang
+external HSA_XNACK=1
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 cd ${REPO_DIR}/Pragma_Examples/OpenMP/CXX/5_device_routines/2_member_function_external/1_member_function_external
 make
