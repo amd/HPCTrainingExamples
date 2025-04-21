806f808a5a8fd25670eb4f9be7ae5cbc74a94ff0
Bob Robey <Bob.Robey@amd.com>
2025-04-21 18:02:31 -0500

    Fixing openmp cxx tests needing HSA_XNACK set

--- tests/openmp_cpp_classes_usm_daxpy.sh
+++ tests/openmp_cpp_classes_usm_daxpy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 module load amdclang
+export HSA_XNACK=1
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 pushd ${REPO_DIR}/Pragma_Examples/OpenMP/CXX/cpp_classes/usm/daxpy
 make 
