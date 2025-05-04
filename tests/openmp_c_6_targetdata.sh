ee3838738da32e2ffb75743071da1b5dab0e2db2
Bob Robey <Bob.Robey@amd.com>
2025-05-04 16:29:31 -0500

    adding more OpenMP C tests

---
+++ tests/openmp_c_6_targetdata.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+module load amdclang
+
+REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/C/1_saxpy/16_targetdata.sh
+
+make
+./saxpy
+make clean
