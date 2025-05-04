a6e0da7eb282bd98bb1d39ce01f054058e3cd5f7
Bob Robey <Bob.Robey@amd.com>
2025-05-04 16:15:46 -0500

    Adding a couple more saxpy C tests

---
+++ tests/openmp_c_0_saxpy_portyourself.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+module load amdclang
+
+REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/C/1_saxpy/0_saxpy_portyourself
+
+make
+./saxpy
+make clean
