75746b1c02707f17c55b4d8ce4c753291af06498
Bob Robey <Bob.Robey@amd.com>
2025-05-04 18:34:06 -0500

    Adding openmp c reduction of two scalars

---
+++ tests/openmp_c_1_reduction_scalars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+module load amdclang
+
+REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/C/4_reduction_scalars/1_reduction_scalars
+
+make
+./reduction_scalar
+make clean
