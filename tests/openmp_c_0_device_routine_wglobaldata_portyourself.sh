a31652d22b33bf260d8d85b77a464f50792547dd
Bob Robey <Bob.Robey@amd.com>
2025-05-04 19:00:14 -0500

    Adding more OpenMP C device routines

---
+++ tests/openmp_c_0_device_routine_wglobaldata_portyourself.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+module load amdclang
+
+REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/C/5_device_routines/2_device_routine_wglobaldata/0_device_routine_wglobaldata_portyourself
+
+make
+./device_routine
+make clean
--- tests/openmp_c_1_device_routine_wglobaldata.sh
+++ tests/openmp_c_1_device_routine_wglobaldata.sh
@@ -3,7 +3,7 @@
 module load amdclang
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-cd ${REPO_DIR}/Pragma_Examples/OpenMP/C/5_device_routines/2_device_routine_wglobaldata
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/C/5_device_routines/2_device_routine_wglobaldata/0_device_routine_wglobaldata
 
 make
 ./device_routine
