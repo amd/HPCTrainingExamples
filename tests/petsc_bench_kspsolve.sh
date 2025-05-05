8cb532128fa3dd6666402a5223231f7f84cbbc01
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2025-05-05 17:20:45 -0500

    Adding petsc fortran test to suite

--- tests/petsc_bench_kspsolve.sh
+++ tests/petsc_bench_kspsolve.sh
@@ -6,9 +6,56 @@
 # to the instructions available in the model installation repo:
 # https://github.com/amd/HPCTrainingDock/blob/main/extras/scripts/petsc_setup.sh
 
+PETSC_MODULE="petsc"
+
+usage()
+{
+    echo ""
+    echo "--help : prints this message"
+    echo "--petsc-module : specifies which petsc module to load"
+    echo ""
+    exit
+}
+
+send-error()
+{
+    usage
+    echo -e "\nError: ${@}"
+    exit 1
+}
+
+reset-last()
+{
+   last() { send-error "Unsupported argument :: ${1}"; }
+}
+
+n=0
+while [[ $# -gt 0 ]]
+do
+   case "${1}" in
+      "--petsc-module")
+          shift
+          PETSC_MODULE=${1}
+          reset-last
+          ;;
+     "--help")
+          usage
+          ;;
+      "--*")
+          send-error "Unsupported argument at position $((${n} + 1)) :: ${1}"
+          ;;
+      *)
+         last ${1}
+         ;;
+   esac
+   n=$((${n} + 1))
+   shift
+done
+
+
 module purge
 
-module load rocm openmpi petsc
+module load rocm openmpi $PETSC_MODULE
 
 PETSC_VERSION=`$PETSC_DIR/lib/petsc/bin/petscversion`
 
