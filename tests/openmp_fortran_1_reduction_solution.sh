7dfd4a371d57f5d54b5b72e8803f0b9eba7b1d3c
Bob Robey <Bob.Robey@amd.com>
2025-05-09 17:27:53 -0500

    Fixing OpenMP Fortran reduction tests

--- tests/openmp_fortran_1_reduction_solution.sh
+++ tests/openmp_fortran_1_reduction_solution.sh
@@ -3,7 +3,7 @@
 module load amdflang-new
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/2_reduction/1_reduction_solution
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/3_reduction/1_reduction_solution
 
 make
 ./freduce
