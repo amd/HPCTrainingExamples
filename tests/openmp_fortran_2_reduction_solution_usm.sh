b08175a9acb14906f7f585aa66c3a06268bf41bb
Bob Robey <Bob.Robey@amd.com>
2025-05-09 17:44:55 -0500

    Fixing OpenMP Fortran reduction tests

--- tests/openmp_fortran_2_reduction_solution_usm.sh
+++ tests/openmp_fortran_2_reduction_solution_usm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 module load amdflang-new
+export HSA_XNACK=1
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/3_reduction/2_reduction_solution_usm
--- tests/openmp_fortran_3_reduction_wrongmapping.sh
+++ tests/openmp_fortran_3_reduction_wrongmapping.sh
@@ -3,7 +3,7 @@
 module load amdflang-new
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/3_reduction/3_reduction_solution_wrongmapping
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/3_reduction/3_reduction_wrongmapping
 
 make
 ./freduce
