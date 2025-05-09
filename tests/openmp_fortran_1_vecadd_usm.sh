0c643a2c6dc40e5d7c008cf4c37478c64a287770
Bob Robey <Bob.Robey@amd.com>
2025-05-09 17:49:28 -0500

    Fixing OpenMP Fortran reduction tests

--- tests/openmp_fortran_1_vecadd_usm.sh
+++ tests/openmp_fortran_1_vecadd_usm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 module load amdflang-new
+export HSA_XNACK=1
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/2_vecadd/1_vecadd_usm
