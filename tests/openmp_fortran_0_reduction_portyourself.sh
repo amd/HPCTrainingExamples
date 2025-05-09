7972c33121af1f3789da7f42a6ba279903198067
Bob Robey <Bob.Robey@amd.com>
2025-05-09 17:37:50 -0500

    Fixing OpenMP Fortran reduction tests

--- tests/openmp_fortran_0_reduction_portyourself.sh
+++ tests/openmp_fortran_0_reduction_portyourself.sh
@@ -6,5 +6,5 @@ REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/3_reduction/0_reduction_portyourself
 
 make
-./freduction
+./freduce
 make clean
