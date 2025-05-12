27ce3bd3afae49f73c538d5b5e3e56a64ea9a8a2
Bob Robey <Bob.Robey@amd.com>
2025-05-12 15:19:20 -0500

    Adding C allocation optimization problem code

---
+++ Pragma_Examples/OpenMP/C/optimization/Allocations/1_alloc_problem/alloc_problem.c
@@ -0,0 +1,60 @@
+// Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+
+#include <omp.h>
+#include <cmath>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+
+#pragma requires unified_shared memory
+
+   // Size of vectors
+   int n = 10000000;
+   int Niter = 10;
+   // Input vectors and Output vector
+   double *a, *b, *c;
+   int i;
+   double sum;
+   double start_time, end_time;
+
+   start_time = omp_get_wtime();
+
+   for (int iter = 0; i < Niter; i++){
+      // Allocate memory for each vector
+      a = (double *) malloc(n*sizeof(double));
+      b = (double *) malloc(n*sizeof(double));
+      c = (double *) malloc(n*sizeof(double));
+
+      // Initialize input vectors
+      #pragma omp target teams loop
+      for (int i = 0; i < n; i++){
+         a[i] = sin((double)i)*sin((double)i);
+         b[i] = cos((double)i)*cos((double)i);
+         c[i] = 0.0;
+      }
+
+      #pragma omp target teams loop
+      for (int i = 0; i < n; i++){
+         c[i] = a[i] + b[i];
+      }
+
+      // Sum up vector c. Print result divided by n. It should equal 1
+      sum = 0.0;
+      #pragma omp target teams loop reduction(+:sum)
+      for (int i = 0; i < n; i++){
+          sum += c[i];
+      }
+
+      sum /= (double)n;
+          
+      free(a);
+      free(b);
+      free(c);
+   }
+    
+   printf("Final result: %lf\n", sum);
+
+   end_time = omp_get_wtime();
+   printf("Runtime is: %lf msecs\n",(end_time - start_time) * 1000.0);
+}
