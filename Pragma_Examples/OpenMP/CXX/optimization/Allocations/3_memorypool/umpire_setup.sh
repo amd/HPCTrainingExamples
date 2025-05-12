79601a26e8a48c50b6b9c5d2c0de38a560cdf8f7
Bob Robey <Bob.Robey@amd.com>
2025-05-12 16:10:48 -0500

    Adding umpire to C++ memory pool example

---
+++ Pragma_Examples/OpenMP/CXX/optimization/Allocations/3_memorypool/umpire_setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+git clone --recursive https://github.com/LLNL/Umpire.git
+cd Umpire
+sed -i 's/memoryType/type/g' src/umpire/tpl/camp/include/camp/resource/hip.hpp
+mkdir -p build && cd build
+UMPIRE_PATH=$HOME/umpire
+mkdir $UMPIRE_PATH
+
+module load rocm
+module load gcc
+
+AMDGPU_GFXMODEL=`rocminfo | grep gfx | sed -e 's/Name://' | head -1 |sed 's/ //g'`
+
+cmake -DCMAKE_INSTALL_PREFIX=${UMPIRE_PATH} \
+      -DROCM_ROOT_DIR=${ROCM_PATH} \
+      -DHIP_ROOT_DIR=${ROCM_PATH}/hip \
+      -DHIP_PATH=${ROCM_PATH}/llvm/bin \
+      -DENABLE_HIP=On \
+      -DENABLE_OPENMP=Off \
+      -DENABLE_CUDA=Off \
+      -DENABLE_MPI=Off \
+      -DCMAKE_CXX_COMPILER=$CXX \
+      -DCMAKE_C_COMPILER=$CC \
+      -DCMAKE_C_COMPILER=$FC \
+      -DCMAKE_HIP_ARCHITECTURES=$AMDGPU_GFXMODEL \
+      -DAMDGPU_TARGETS=$AMDGPU_GFXMODEL \
+      -DCMAKE_HIP_ARCHITECTURES=$AMDGPU_GFXMODEL \
+      -DGPU_TARGETS=$AMDGPU_GFXMODEL \
+      -DBLT_CXX_STD=c++14 \
+      -DUMPIRE_ENABLE_IPC_SHARED_MEMORY=On \
+      -DENABLE_FORTRAN=On \
+      ../
+
+make -j 16
+
+make install
