66b3cd903c2db171dff6d2b00d45307df13b2f1d
Bob Robey <Bob.Robey@amd.com>
2025-05-12 20:09:04 -0500

    Adding openmp_optimizations test

---
+++ tests/openmp_optimizations_cxx_1_alloc_problem.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+module load amdclang
+export HSA_XNACK=1
+
+REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/CXX/optimization/Allocations/1_alloc_problem
+
+make
+./alloc_problem
+make clean
