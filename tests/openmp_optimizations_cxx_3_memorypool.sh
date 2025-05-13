436f16ace87de75db0d90d0f95df6df689f2fa5c
Bob Robey <Bob.Robey@amd.com>
2025-05-13 08:42:36 -0500

    Adding C++ memory pool example test

---
+++ tests/openmp_optimizations_cxx_3_memorypool.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+module load amdclang
+export HSA_XNACK=1
+
+REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/CXX/optimization/Allocations/3_memorypool
+
+./umpire_setup.sh
+export UMPIRE_PATH=${PWD}/Umpire_install
+make
+./memorypool
+make clean
+
+rm -rf Umpire_source Umpire_install
