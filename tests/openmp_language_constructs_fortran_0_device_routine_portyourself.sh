99ef2c1b6b7e60014fa8c0cb1b0461f6a31684bf
Bob Robey <Bob.Robey@amd.com>
2025-05-15 17:03:01 -0500

    Adding OpenMP Fortran device routine

--- tests/openmp_language_constructs_fortran_0_device_routine_portyourself.sh
+++ tests/openmp_language_constructs_fortran_0_device_routine_portyourself.sh
@@ -2,7 +2,7 @@
 
 module load amdclang
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/ 5_device_routines/device_routine_with_interface/0_device_routine_portyourself
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/5_device_routines/device_routine_with_interface/0_device_routine_portyourself
 make
 ./device_routine
 
