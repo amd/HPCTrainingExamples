78b7cc938ad46fd4b5da7b8723622ee70282156c
Bob Robey <Bob.Robey@amd.com>
2025-05-24 11:56:51 -0500

    Adding MPI Ghost Exchange test

--- tests/mpi_ghost_exchange_ver1.sh
+++ tests/mpi_ghost_exchange_ver1.sh
@@ -15,12 +15,12 @@ make
 
 NUMCPUS=`lscpu | grep '^CPU(s):' |cut -d':' -f2 | tr -d ' '`
 NUM_GPUS=`rocminfo |grep GPU |grep "Device Type" |wc -l`
-NUM_PER_RESOURCE_MPI4=`expr 4 / NUM_GPUS`
-NUM_PER_RESOURCE_MPI16=`expr 16 / NUM_GPUS`
+NUM_PER_RESOURCE_MPI4=`expr 4 / ${NUM_GPUS}`
+NUM_PER_RESOURCE_MPI16=`expr 16 / ${NUM_GPUS}`
 
-mpirun -n 4 --bind-to core --map-by ppr:${NUM_PER_RESOURCE_MPI4}:numa p--report-bindings ./GhostExchange \
+mpirun -n 4 --bind-to core --map-by ppr:${NUM_PER_RESOURCE_MPI4}:numa  --report-bindings ./GhostExchange \
        -x 2  -y 2  -i 2000 -j 2000 -h 2 -t -c -I 1000
-if [ ${NUM_PER_RESOUCE_MPI16} -le 4 ]; then
+if [[ ${NUM_PER_RESOUCE_MPI16} -le 4 ]]; then
    mpirun -n 16 --bind-to core --map-by ppr:${NUM_PER_RESOURCE_MPI16}:numa  --report-bindings ./GhostExchange \
           -x 4  -y 4  -i 20000 -j 20000 -h 2 -t -c -I 1000
 fi
