42f034b60d9ce97fb3e6f6ba66b831156e6023de
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2025-07-16 09:53:07 -0500

    Adding more tests by Mashy to test suite

---
+++ tests/advancedopenmp_fortran_memory2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+module load rocm
+GFX_MODEL=`rocminfo | grep gfx | sed -e 's/Name://' | head -1 |sed 's/ //g'`
+if [ "${GFX_MODEL}" = "gfx1030" ] ; then
+   echo "Skip"
+else
+   module load amdflang-new
+
+   REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+   cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/memory_pragmas
+
+   export LIBOMPTARGET_INFO=-1
+   export LIBOMPTARGET_INFO_SUPPORT=0
+   export SLURM_BATCH_WAIT=0
+   export OMP_TARGET_OFFLOAD=MANDATORY
+
+   rm -rf build
+   mkdir build && cd build
+   cmake ..
+   make mem2
+   ./mem2
+
+   cd ..
+   rm -rf build
+fi
---
+++ tests/openmp_fortran_atomic_usm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+module load amdflang-new
+REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/Atomic
+export HSA_XNACK=1
+make
+./atomic
+
+make clean
