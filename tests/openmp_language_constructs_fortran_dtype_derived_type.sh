e83479d01b9a8081edc512ac38de5ae02c68dcb3
Bob Robey <Bob.Robey@amd.com>
2025-07-19 11:52:00 -0500

    Fixing another Fortran test directory change

--- tests/openmp_language_constructs_fortran_dtype_derived_type.sh
+++ tests/openmp_language_constructs_fortran_dtype_derived_type.sh
@@ -3,7 +3,7 @@
 module load amdflang-new
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran
-cd derived_types
+cd 6_derived_types
 make dtype_derived_type
 ./dtype_derived_type
 
