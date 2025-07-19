82d95e19d139435ad87d016e54695d3a210c8375
Bob Robey <Bob.Robey@amd.com>
2025-07-19 11:55:19 -0500

    Adding HSA_XNACK to test

--- tests/openmp_c_single_line_constructs_saxpy_gpu_collapse.sh
+++ tests/openmp_c_single_line_constructs_saxpy_gpu_collapse.sh
@@ -5,6 +5,8 @@ module load amdclang
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 cd ${REPO_DIR}/Pragma_Examples/OpenMP/C/SingleLineConstructs
 
+export HSA_XNACK=1
+
 make
 ./saxpy_gpu_collapse
 make clean
