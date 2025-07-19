1793f1aa260a8def4250fe7a66efeaa08486214d
Bob Robey <Bob.Robey@amd.com>
2025-07-19 13:18:01 -0500

    Fixing more fortran derived type tests

---
+++ tests/openmp_language_constructs_fortran_dtype_derived_type_usm.f
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+module load amdflang-new
+REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran
+cd 6_derived_types
+make dtype_derived_type_usm
+./dtype_derived_type_usm
+
+make clean
