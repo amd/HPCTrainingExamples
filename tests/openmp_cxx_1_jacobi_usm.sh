8e5e25e6fdf976f3e23f97414c4c1d562b7e24e7
Bob Robey <Bob.Robey@amd.com>
2025-07-19 12:10:57 -0500

    Fixing directory names

--- tests/openmp_cxx_1_jacobi_usm.sh
+++ tests/openmp_cxx_1_jacobi_usm.sh
@@ -3,7 +3,7 @@
 module load amdclang
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-cd ${REPO_DIR}/Pragma_Examples/OpenMP/C/7_jacobi/1_jacobi_usm
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/CXX/7_jacobi/1_jacobi_usm
 
 export HSA_XNACK=1
 make
