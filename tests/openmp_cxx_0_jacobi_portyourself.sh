b45677f25dbe96c85d1eda83f01bb3035325d119
Bob Robey <Bob.Robey@amd.com>
2025-07-19 13:54:42 -0500

    Fixing jacobi makefile

--- tests/openmp_cxx_0_jacobi_portyourself.sh
+++ tests/openmp_cxx_0_jacobi_portyourself.sh
@@ -5,6 +5,6 @@ module load amdclang
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 cd ${REPO_DIR}/Pragma_Examples/OpenMP/CXX/7_jacobi/0_jacobi_portyourself
 
-make
+make CC=$CXX
 ./jacobi
 make clean
