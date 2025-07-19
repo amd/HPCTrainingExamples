56c46f66512abdb2cd7cc3db31eecba74e79830a
Bob Robey <Bob.Robey@amd.com>
2025-07-19 14:58:00 -0500

    Fixing jacobi expected results

--- tests/openmp_language_constructs_fortran_dtype_derived_type_automap.sh
+++ tests/openmp_language_constructs_fortran_dtype_derived_type_automap.sh
@@ -4,6 +4,7 @@ module load amdflang-new
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran
 cd 6_derived_types
+export HSA_XNACK=1
 make dtype_derived_type_automap
 ./dtype_derived_type_automap
 
