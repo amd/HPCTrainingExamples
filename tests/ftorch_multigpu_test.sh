64f7273d7452f0bef96ecc79c18901af0e567dc7
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2025-07-21 02:45:24 -0500

    Fixed ftorch test

--- tests/ftorch_multigpu_test.sh
+++ tests/ftorch_multigpu_test.sh
@@ -6,22 +6,20 @@
 # to the instructions available in the model installation repo:
 # https://github.com/amd/HPCTrainingDock/blob/main/extras/scripts/ftorch_setup.sh
 
-
-
 module load ftorch
 
 git clone https://github.com/Cambridge-ICCS/FTorch.git ftorch_test
-cd ftorch_test/examples/6_MultiGPU
+cd ftorch_test/examples/
+python3 -m venv ftorch_test
+source ftorch_test/bin/activate
+cd 6_MultiGPU
 python3 pt2ts.py --device_type hip
 python3 multigpu_infer_python.py --device_type hip
 mkdir build && cd build
 cmake ..
 make -j
 ./multigpu_infer_fortran hip ../saved_multigpu_model_hip.pt
-
-cd ../..
+deactivate
+cd ../../../../
 rm -rf ftorch_test
-
-
-
 
