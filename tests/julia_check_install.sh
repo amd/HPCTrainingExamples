f95924a5f53e18f690f720d391de1455d8de062e
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2025-07-21 07:07:09 -0500

    Modified Julia tests to not use the module

--- tests/julia_check_install.sh
+++ tests/julia_check_install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# This test checks that the AMDGPU is visible from Julia 
+# This test installs Julia in $PWD/.julia 
 
-# NOTE: this test assumes Julia has been installed according
-# to the instructions available in the model installation repo:
+# NOTE: this test uses the instructions located at 
 # https://github.com/amd/HPCTrainingDock/blob/main/extras/scripts/julia_setup.sh
+
 
 export CUR_DIR=$PWD
 curl -fsSL https://install.julialang.org | sh -s -- --yes --add-to-path=no -p=${CUR_DIR}/juliaup_install
 export PATH=$PATH:"${CUR_DIR}/juliaup_install/bin"
 juliaup add 1.12
 juliaup default 1.12
-julia -e 'using Pkg; Pkg.add("AMDGPU")'
-julia -e 'using AMDGPU;display(AMDGPU.devices())'
+juliaup status
 rm -rf $HOME/.julia ${CUR_DIR}/juliaup_install
 
--- tests/julia_gpu_vec_add.sh
+++ tests/julia_gpu_vec_add.sh
@@ -6,9 +6,15 @@
 # to the instructions available in the model installation repo:
 # https://github.com/amd/HPCTrainingDock/blob/main/extras/scripts/julia_setup.sh
 
-module load julia
-
+export CUR_DIR=$PWD
+curl -fsSL https://install.julialang.org | sh -s -- --yes --add-to-path=no -p=${CUR_DIR}/juliaup_install
+export PATH=$PATH:"${CUR_DIR}/juliaup_install/bin"
+juliaup add 1.12
+juliaup default 1.12
+julia -e 'using Pkg; Pkg.add("AMDGPU")'
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 pushd $REPO_DIR/Julia/vec_add
 julia vec_add.jl
 popd
+rm -rf $HOME/.julia ${CUR_DIR}/juliaup_install
+
