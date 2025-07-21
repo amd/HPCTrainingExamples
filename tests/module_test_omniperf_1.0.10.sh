56248c6cb8834ea40a61b1e69483059ab48a748d
Bob Robey <Bob.Robey@amd.com>
2025-07-21 10:08:07 -0500

    Removing old module tests

--- tests/module_test_omniperf_1.0.10.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ls -l  /opt/rocmplux-* |grep omniperf-1.0.10
-
-module load rocm omniperf/1.0.10
-module list
-
-omniperf --version
--- tests/module_test_omniperf_1.1.0-PR1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ls -l /opt/rocmplus-* | grep omniperf-1.1.0-PR1
-
-module load rocm omniperf/1.1.0-PR1
-module list
-
-omniperf --version
