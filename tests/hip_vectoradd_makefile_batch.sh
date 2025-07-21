c3e4450dc962b896248fd887dd4497f85e660a19
Bob Robey <Bob.Robey@amd.com>
2025-07-21 18:14:00 -0500

    Fixes for batch tests

--- tests/hip_vectoradd_makefile_batch.sh
+++ tests/hip_vectoradd_makefile_batch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-sbatch --wait ${REPO_DIR}/HIP/vectorAdd/hip_makefile_batch.sh
+sinfo
+sbatch --wait --time 3 ${REPO_DIR}/HIP/vectorAdd/hip_makefile_batch.sh
 
 grep PASSED! slurm-*.out
 rm  slurm-*.out
