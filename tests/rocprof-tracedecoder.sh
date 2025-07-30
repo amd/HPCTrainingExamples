bc01a1021f9b5c0c55d64bf3d1ce0ab4a535a378
Bob Robey <Bob.Robey@amd.com>
2025-07-29 20:11:26 -0500

    modifying tracedecoder test

--- tests/rocprof-tracedecoder.sh
+++ tests/rocprof-tracedecoder.sh
@@ -8,9 +8,11 @@ cd ${REPO_DIR}/HIP/vectorAdd
 make vectoradd
 ./vectoradd
 rocprofv3 --att -d out -- ./vectoradd
-if [ -f out/ui_output_agent_*_dispatch_1/se0_sm0_sl0_wv0.json ]; then
+numfiles=`ls -l out/ui_output_agent_*_dispatch_1 |wc -l`
+if [[ "$numfiles" -gt 5 ]]; then
   echo "Found a json output from the rocprofv3 compute viewer and trace decoder"
-  ls -l out/ui_output_agent_*_dispatch_1/se0_sm0_sl0_wv0.json
+  ls -l out/ui_output_agent_*_dispatch_1/
   echo "PASSED"
 fi
 make clean
+rm -rf out
