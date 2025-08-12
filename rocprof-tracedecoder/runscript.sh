a7117c5c76d715d0339603f2c4d3a70ff64ae175
Bob Robey <Bob.Robey@amd.com>
2025-08-12 08:59:30 -0500

    More fixes to trace decoder tests

--- rocprof-tracedecoder/runscript.sh
+++ rocprof-tracedecoder/runscript.sh
@@ -6,23 +6,24 @@ cd ../HIP/vectorAdd
 #Edit the Makefile and add -g to the HIPFlags
 make vectoradd
 rocprofv3 --att --att-activity 16 -d tracedecoder_vectorAdd -- ./vectoradd
-tar -cf ../../tracedecoder.tar tracedecoder_vectorAdd
+tar -cvf ../../tracedecoder.tar tracedecoder_vectorAdd
 make clean
 rm -rf tracedecoder_vectorAdd
 
 cd ../saxpy
 make saxpy
 rocprofv3 --att --att-activity 16 -d tracedecoder_saxpy -- ./saxpy
-tar -rf ../../tracedecoder.tar tracedecoder_saxpy
+tar -rvf ../../tracedecoder.tar tracedecoder_saxpy
 make clean
 rm -rf tracedecoder_saxpy
 
 cd ../dgemm
+rm -rf build
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
-make
+make VERBOSE=1
 rocprofv3 --att --att-activity 16 -d tracedecoder_dgemm_hip -- bin/dgemm -m 8192 -n 8192 -k 8192 -i 3 -r 10 -d 0,1,2,3 -o dgemm.csv
-tar -rf ../../tracedecoder.tar tracedecoder_dgemm_hip
+tar -rvf ../../../tracedecoder.tar tracedecoder_dgemm_hip
 make clean
 rm -rf tracedecoder_dgemm_hip
 cd ..
@@ -31,6 +32,6 @@ rm -rf build
 cd ../../rocprof-tracedecoder/dgemm
 make
 rocprofv3 --att --att-activity 16 -d tracedecoder_dgemm_library -- ./dgemm
-tar -rf ../../tracedecoder.tar tracedecoder_dgemm_library
+tar -rvf ../../tracedecoder.tar tracedecoder_dgemm_library
 make clean
 rm -rf tracedecoder_dgemm_library
