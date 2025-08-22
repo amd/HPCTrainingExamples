f04224c5d3bd2e8e5a455cf90d78a2473d0a2ef9
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2025-08-22 14:58:22 -0500

    Changes to netcdf fortran env variables in test

--- tests/netcdf-fortran_simple_xy_par.sh
+++ tests/netcdf-fortran_simple_xy_par.sh
@@ -17,7 +17,7 @@ rm -rf netcdf-fortran_test
 mkdir netcdf-fortran_test
 cd netcdf-fortran_test
 git clone https://github.com/Unidata/netcdf-fortran.git
-$FC  ./netcdf-fortran/examples/F90/simple_xy_par_wr.F90 -o simple_xy_par_wf  -I${NETCDF_FC_ROOT}/include -L${NETCDF_FC_ROOT}/lib -lnetcdff -L${PNETCDF_ROOT}/lib -lpnetcdf
+$FC  ./netcdf-fortran/examples/F90/simple_xy_par_wr.F90 -o simple_xy_par_wf  -I${NETCDF_F_ROOT}/include -L${NETCDF_F_ROOT}/lib -lnetcdff -L${PNETCDF_ROOT}/lib -lpnetcdf
 mpirun -n 4 ./simple_xy_par_wf
 ncdump simple_xy_par.nc
 cd ..
