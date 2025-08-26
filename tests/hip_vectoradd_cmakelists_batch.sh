1acef2f3b5921faeb9f85485faf229f851ace890
Bob Robey <Bob.Robey@amd.com>
2025-08-25 19:33:29 -0500

    Fixing batch tests

--- tests/hip_vectoradd_cmakelists_batch.sh
+++ tests/hip_vectoradd_cmakelists_batch.sh
@@ -3,7 +3,8 @@ REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 if [[ `sinfo | grep unk | wc -l` == 1 ]]; then
    echo "SLURM not configured properly -- SKIPPING"
 else
-   sbatch --wait ${REPO_DIR}/HIP/vectorAdd/hip_cmakelists_batch.sh
+   cd ${REPO_DIR}/HIP/vectorAdd
+   sbatch --wait hip_cmakelists_batch.sh
 
    grep PASSED! slurm-*.out
    rm  slurm-*.out
