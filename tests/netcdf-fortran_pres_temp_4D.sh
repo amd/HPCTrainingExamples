607e0fcf85c74c9d86057776d53c7674f7c58771
Bob Robey <Bob.Robey@amd.com>
2025-08-25 22:54:45 -0500

    Fixes for netcdf-fortran_pres problem

--- tests/netcdf-fortran_pres_temp_4D.sh
+++ tests/netcdf-fortran_pres_temp_4D.sh
@@ -10,8 +10,8 @@ rm -rf netcdf-fortran_test
 mkdir netcdf-fortran_test
 cd netcdf-fortran_test
 git clone https://github.com/Unidata/netcdf-fortran.git
-$FC ./netcdf-fortran/examples/F90/pres_temp_4D_wr.F90 -I${NETCDF_F_ROOT}/include -L${NETCDF_F_ROOT}/lib -lnetcdff -o pres_temp_4D_wr
-$FC ./netcdf-fortran/examples/F90/pres_temp_4D_rd.F90 -I${NETCDF_F_ROOT}/include -L${NETCDF_F_ROOT}/lib -lnetcdff -o pres_temp_4D_rd
+$FC ./netcdf-fortran/examples/F90/pres_temp_4D_wr.F90 -I${NETCDF_F_ROOT}/include -L${NETCDF_F_ROOT}/lib -lnetcdff -L${NETCDF_C_ROOT}/lib -lnetcdf -o pres_temp_4D_wr
+$FC ./netcdf-fortran/examples/F90/pres_temp_4D_rd.F90 -I${NETCDF_F_ROOT}/include -L${NETCDF_F_ROOT}/lib -lnetcdff -L${NETCDF_C_ROOT}/lib -lnetcdf -o pres_temp_4D_rd
 ./pres_temp_4D_wr
 ./pres_temp_4D_rd
 cd ..
