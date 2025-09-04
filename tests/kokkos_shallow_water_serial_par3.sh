08cf780eae9e6c73c31a8afb0cd054f1a895a1c4
Bob Robey <Bob.Robey@amd.com>
2025-09-04 10:11:28 -0500

    Fixing cleanup in kokkos test

--- tests/kokkos_shallow_water_serial_par3.sh
+++ tests/kokkos_shallow_water_serial_par3.sh
@@ -3,13 +3,14 @@
 module load gcc
 module load rocm
 git clone --recursive https://github.com/EssentialsOfParallelComputing/Chapter13 Chapter13
-cd Chapter13/Kokkos/ShallowWater
+pushd Chapter13/Kokkos/ShallowWater
 
 mkdir serial_build; cd serial_build
 cmake ..
 make -j 8 ShallowWater_par3
 
 ./ShallowWater_par3
+cd ..
 
-cd ../../../..
+popd
 rm -rf Chapter13
