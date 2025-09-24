fd52706b837ad47bae35c50ca507e445cdab796f
Bob Robey <Bob.Robey@amd.com>
2025-09-23 20:20:12 -0500

    Adding hip-python test

---
+++ tests/hip-python_check_import.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This test imports the hip-python package in Python to test
+# if HIP-Python is installed and accessible
+
+# NOTE: this test assumes HIP-Python has been installed according
+# to the instructions available in the model installation repo:
+# https://github.com/amd/HPCTrainingDock/blob/main/extras/scripts/hip-python_setup.sh
+
+
+module load rocm
+module load hip-python
+
+python3 -c 'import hip' 2> /dev/null && echo 'Success' || echo 'Failure'
