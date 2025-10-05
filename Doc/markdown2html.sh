055dd6878316a998037b842221a8d46e95adf7b9
Bob Robey <Bob.Robey@amd.com>
2025-10-05 12:21:58 -0500

    Fixing markdown to html

--- Doc/markdown2html.sh
+++ Doc/markdown2html.sh
@@ -3,20 +3,21 @@
 if [ $# -ne 1 ]
 then
 	echo "Requires one argument with filename of markdown file"
-	echo "Usage: ./markdown2html.sh test.md"
+	echo "Usage: ./markdown2html.sh event_name"
 fi
 
 if [ -z "$1" ]
 then
 	echo "Requires one argument with filename of markdown file"
-	echo "Usage: ./markdown2html.sh test.md"
+	echo "Usage: ./markdown2html.sh event_name"
 fi
 
-if [ ! -f "$1" ]
+if [ ! -f "$1.md" ]
 then
 	echo "file $1 does not exist"
-	echo "Usage: ./markdown2html.sh test.md"
+	echo "Usage: ./markdown2html.sh event_name"
 fi
 
-filename="${1%.*}"
-pandoc "$1" -V geometry:margin=1in --metadata title="$filename" --toc -s -o "$filename.html"
+filename="${1}"
+EVENT_NAME=`echo ${1} | sed -e 's/_/ /g' `
+pandoc "$filename.md" -V geometry:margin=1in --metadata title="$EVENT_NAME" --toc -s -o "$filename.html"
