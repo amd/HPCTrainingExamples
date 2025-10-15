8140e5007e763dde157719f3c6ee1692fa6710ac
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2025-10-15 09:28:58 -0500

    Added Deepspeed import check

---
+++ tests/deepspeed_check_import.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This test imports the deepspeed  package in Python
+# to tests if it is installed and accessible
+
+# NOTE: this test assumes deepspeed has been installed according
+# to the instructions available in the model installation repo:
+# https://github.com/amd/HPCTrainingDock/blob/main/extras/scripts/pytorch_setup.sh
+
+
+
+module load rocm pytorch
+
+python3 -c 'import deepspeed' 2> /dev/null && echo 'Success' || echo 'Failure'
+
+
