8d48589e298a11c319dfff7965d42f51e9b65e2f
Johanna Potyka <168717070+jopotyka@users.noreply.github.com>
2025-10-23 07:08:30 +0200

    Fix tests (#127)
    * fix for amdflang but will break cray ftn
    
    * fix tests

--- tests/openmp_c_3_saxpy_teamsdistribute.sh
+++ tests/openmp_c_3_saxpy_teamsdistribute.sh
@@ -5,7 +5,7 @@ module load amdclang
 export HSA_XNACK=1
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-cd ${REPO_DIR}/Pragma_Examples/OpenMP/C/1_saxpy/3_saxpy_teamsdistribute
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/C/1_saxpy/2_saxpy_teamsdistribute
 
 make
 ./saxpy
--- tests/openmp_fortran_1_reduction_solution.sh
+++ tests/openmp_fortran_1_reduction_solution.sh
@@ -4,7 +4,7 @@ module load rocm
 module load amdflang-new
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
-cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/3_reduction/1_reduction_solution
+cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/3_reduction/2_reduction_solution
 
 make
 ./freduce
