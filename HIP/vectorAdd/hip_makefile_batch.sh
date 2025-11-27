7df3c0462ebcd241f098b9c058a9c155767c328d
Bob Robey <Bob.Robey@amd.com>
2025-11-27 14:01:24 -0600

    Using already loaded rocm instead of default

--- HIP/vectorAdd/hip_makefile_batch.sh
+++ HIP/vectorAdd/hip_makefile_batch.sh
@@ -3,7 +3,11 @@
 #SBATCH --gpus=1
 #SBATCH -t 10:00
 
-module load rocm
+if ! module is-loaded "rocm"; then
+  echo "rocm module is not loaded"
+  echo "loading default rocm module"
+  module load rocm
+fi
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 cd $REPO_DIR/../HIP/vectorAdd
 
