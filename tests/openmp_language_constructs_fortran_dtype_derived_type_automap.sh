7866b7693d6939f0c0091f9d42707a18697b9f31
Bob Robey <Bob.Robey@amd.com>
2026-01-27 17:21:55 -0600

    Fixing more tests for HPE systems

--- tests/openmp_language_constructs_fortran_dtype_derived_type_automap.sh
+++ tests/openmp_language_constructs_fortran_dtype_derived_type_automap.sh
@@ -18,7 +18,7 @@ if [ "$?" == "1" ]; then
 fi
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran
-cd 6_derived_types
+cd 7_derived_types
 export HSA_XNACK=1
 make dtype_derived_type_automap
 ./dtype_derived_type_automap
