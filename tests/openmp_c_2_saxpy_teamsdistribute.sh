abd96872a7f05a4e5d90ad1a5468859db5379b79
Bob Robey <Bob.Robey@amd.com>
2026-01-27 14:50:29 -0600

    Fixing more tests for HPE systems

--- tests/openmp_c_2_saxpy_teamsdistribute.sh
+++ tests/openmp_c_2_saxpy_teamsdistribute.sh
@@ -7,13 +7,8 @@ if [ $? -eq 1 ]; then
   module load rocm
 fi
 if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
-   if [[ "`module list |& grep PrgEnv-cray | wc -l`" -ge 1 ]]; then
-      export CXX=`which CC`
-      export CC=`which cc`
-   elif [[ "`module list |& grep PrgEnv-amd | wc -l`" -ge 1 ]]; then
-      export CXX=${ROCM_PATH}/llvm/bin/amdclang++
-      export CC=${ROCM_PATH}/llvm/bin/amdclang
-   fi
+   export CXX=`which CC`
+   export CC=`which cc`
 else
    module load amdclang
 fi
