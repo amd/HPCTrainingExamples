7c97bc30d98912cae86f696808e75c00d804de68
Bob Robey <Bob.Robey@amd.com>
2026-01-28 17:18:40 -0600

    Fixing more openmp tests for amd compiler on HPE system

--- tests/openmp_fortran_3_saxpy_paralleldosimd.sh
+++ tests/openmp_fortran_3_saxpy_paralleldosimd.sh
@@ -16,6 +16,7 @@ if [ "$?" == "1" ]; then
       module load amdclang
    fi
 fi
+export HSA_XNACK=1
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/1_saxpy/3_saxpy_paralleldosimd
