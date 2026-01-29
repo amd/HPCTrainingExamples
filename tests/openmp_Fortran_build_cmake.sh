577e70460998c1b9cf33f9f1431b6e31071a76a0
Bob Robey <Bob.Robey@amd.com>
2026-01-29 17:35:51 -0600

    Trying new module loads

--- tests/openmp_Fortran_build_cmake.sh
+++ tests/openmp_Fortran_build_cmake.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 
-module list 2>&1 | grep -q -w "rocm"
-if [ $? -eq 1 ]; then
-  echo "rocm module is not loaded"
-  echo "loading default rocm module"
-  module load rocm
+if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
+   if [ -z "$CXX" ]; then
+      export CXX=`which CC`
+   fi
+   if [ -z "$CC" ]; then
+      export CC=`which cc`
+   fi
+   if [ -z "$FC" ]; then
+      export FC=`which ftn`
+   fi
+else
+   module list 2>&1 | grep -q -w "rocm"
+   if [ $? -eq 1 ]; then
+     echo "rocm module is not loaded"
+     echo "loading default rocm module"
+     module load rocm
+   fi
+   module load amdflang-new >& /dev/null
+   if [ "$?" == "1" ]; then
+      module load amdclang
+   fi
 fi
+
 XNACK_COUNT=`rocminfo | grep xnack | wc -l`
 if [ ${XNACK_COUNT} -lt 1 ]; then
    echo "Skip"
 else
    export HSA_XNACK=1
-
-   if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
-      # For Cray system, set compiler variables
-      export CXX=`which CC`
-      export CC=`which cc`
-      export FC=`which ftn`
-   fi
-   if [[ "`module list |& grep -w PrgEnv-cray |wc -l`" -lt 1 ]]; then
-      # For AMD compilers, allow override
-      if [[ "`module avail |& grep -w amdflang-new |wc -l`" -ge 1 ]]; then
-         if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
-            module unload rocm
-            module switch amd amdflang-new
-         else
-            module load amdflang-new
-         fi
-      else
-         module load amdclang
-      fi
-   fi
 
    REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
    cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/BuildExamples
