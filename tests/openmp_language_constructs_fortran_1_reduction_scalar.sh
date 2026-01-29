fdb0252311a7dc1b57660deb444c58a67af402f3
Bob Robey <Bob.Robey@amd.com>
2026-01-29 15:57:14 -0600

    Trying new module loads

--- tests/openmp_language_constructs_fortran_1_reduction_scalar.sh
+++ tests/openmp_language_constructs_fortran_1_reduction_scalar.sh
@@ -6,17 +6,27 @@ if [ $? -eq 1 ]; then
   echo "loading default rocm module"
   module load rocm
 fi
-module load amdflang-new >& /dev/null
-if [ "$?" == "1" ]; then
-   if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
-      export CXX=`which CC`
-      export CC=`which cc`
-      export FC=`which ftn`
+
+if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
+   # For Cray system, set compiler variables
+   export CXX=`which CC`
+   export CC=`which cc`
+   export FC=`which ftn`
+fi
+if [[ "`module list |& grep -w PrgEnv-cray |wc -l`" -lt 1 ]]; then
+   # For AMD compilers, allow override
+   if [[ "`module avail |& grep -w amdflang-new |wc -l`" -ge 1 ]]; then
+      if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
+         module unload rocm
+         module switch amd amdflang-new
+      else
+         module load amdflang-new
+      fi
    else
       module load amdclang
    fi
-   module load amdclang
 fi
+
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/4_reduction_scalars/1_reduction_scalar_solution
 make
