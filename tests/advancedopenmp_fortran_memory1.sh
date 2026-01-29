4345afb830ec56ce0e39ee7956efbda71ac89e71
Bob Robey <Bob.Robey@amd.com>
2026-01-29 16:38:44 -0600

    Trying new module loads

--- tests/advancedopenmp_fortran_memory1.sh
+++ tests/advancedopenmp_fortran_memory1.sh
@@ -10,12 +10,22 @@ GFX_MODEL=`rocminfo | grep gfx | sed -e 's/Name://' | head -1 |sed 's/ //g'`
 if [ "${GFX_MODEL}" = "gfx1030" ] ; then
    echo "Skip"
 else
-   module load amdflang-new >& /dev/null
-   if [ "$?" == "1" ]; then
-      if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
-         export CXX=`which CC`
-         export CC=`which cc`
-         export FC=`which ftn`
+
+   if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
+      # For Cray system, set compiler variables
+      export CXX=`which CC`
+      export CC=`which cc`
+      export FC=`which ftn`
+   fi
+   if [[ "`module list |& grep -w PrgEnv-cray |wc -l`" -lt 1 ]]; then
+      # For AMD compilers, allow override
+      if [[ "`module avail |& grep -w amdflang-new |wc -l`" -ge 1 ]]; then
+         if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
+            module unload rocm
+            module switch amd amdflang-new
+         else
+            module load amdflang-new
+         fi
       else
          module load amdclang
       fi
