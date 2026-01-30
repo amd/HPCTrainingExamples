ec55ef071c8fed9608d93032957f5bcc19d351a8
Bob Robey <Bob.Robey@amd.com>
2026-01-30 04:43:03 -0600

    More fixes for HPE testing

--- tests/openmp_intro_saxpy4.sh
+++ tests/openmp_intro_saxpy4.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-module list 2>&1 | grep -q -w "rocm"
-if [ $? -eq 1 ]; then
-  echo "rocm module is not loaded"
-  echo "loading default rocm module"
-  module load rocm
-fi
 if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
-   export CXX=`which CC`
-   export CC=`which cc`
-   export FC=`which ftn`
+   if [ -z "$CXX" ]; then
+      export CXX=`which CC`
+   fi
+   if [ -z "$CC" ]; then
+      export CC=`which cc`
+   fi
+   if [ -z "$FC" ]; then
+      export FC=`which ftn`
+   fi
 else
-   module load amdclang
+   module list 2>&1 | grep -q -w "rocm"
+   if [ $? -eq 1 ]; then
+     echo "rocm module is not loaded"
+     echo "loading default rocm module"
+     module load rocm
+   fi
+   module load amdflang-new >& /dev/null
+   if [ "$?" == "1" ]; then
+      module load amdclang
+   fi
 fi
+
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 cd ${REPO_DIR}/Pragma_Examples/OpenMP/Intro
 make saxpy4
