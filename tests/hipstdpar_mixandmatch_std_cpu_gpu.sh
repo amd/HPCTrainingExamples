5dde9d32932c32d1d8053989000b2e37c157cf93
Bob Robey <Bob.Robey@amd.com>
2026-01-29 20:30:26 -0600

    Trying new module loads

--- tests/hipstdpar_mixandmatch_std_cpu_gpu.sh
+++ tests/hipstdpar_mixandmatch_std_cpu_gpu.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-export HSA_XNACK=1
-module list 2>&1 | grep -q -w "rocm"
-if [ $? -eq 1 ]; then
-  echo "rocm module is not loaded"
-  echo "loading default rocm module"
-  module load rocm
-fi
 if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
-   export CXX=`which CC`
+   if [ -z "$CXX" ]; then
+      export CXX=`which CC`
+   fi
+   if [ -z "$CC" ]; then
+      export CC=`which cc`
+   fi
+   if [ -z "$FC" ]; then
+      export FC=`which ftn`
+   fi
+   if [ -z "$HIPCC" ]; then
+      export HIPCC=`which hipcc`
+   fi
+else
+   module list 2>&1 | grep -q -w "rocm"
+   if [ $? -eq 1 ]; then
+     echo "rocm module is not loaded"
+     echo "loading default rocm module"
+     module load rocm
+   fi
+   module load amdflang-new >& /dev/null
+   if [ "$?" == "1" ]; then
+      module load amdclang
+   fi
+fi
+
+if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/cray/pe/cdst-support/2.14.6/lib:${ROCM_PATH}/lib/rocprofiler-systems"
    export LIBS="${LIBS} -L/opt/cray/pe/cdst-support/2.14.6/lib -L${ROCM_PATH}/lib/rocprofiler-systems"
-else
-   module load amdclang
 fi
+
+export HSA_XNACK=1
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 pushd ${REPO_DIR}/HIPStdPar/CXX/MixAndMatch/std_cpu_gpu
