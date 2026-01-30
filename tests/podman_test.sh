c6b4e4ff02776180de15f1733b3d1f1c0ef16114
Bob Robey <Bob.Robey@amd.com>
2026-01-30 10:56:12 -0600

    More fixes for HPE testing

--- tests/podman_test.sh
+++ tests/podman_test.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-module list 2>&1 | grep -q -w "rocm"
-if [ $? -eq 1 ]; then
-  echo "rocm module is not loaded"
-  echo "loading default rocm module"
-  module load rocm
+
+if [[ "`printenv |grep -w CRAY |wc -l`" -lt 1 ]]; then
+   module list 2>&1 | grep -q -w "rocm"
+   if [ $? -eq 1 ]; then
+     echo "rocm module is not loaded"
+     echo "loading default rocm module"
+     module load rocm
+   fi
+   module load amdflang-new >& /dev/null
+   if [ "$?" == "1" ]; then
+      module load amdclang
+   fi
 fi
+
 podman run --device=/dev/dri --device=/dev/kfd  --network=host --ipc=host --group-add keep-groups -v $HOME:/workdir --workdir /workdir docker://rocm/dev-ubuntu-22.04:6.4.1 rocminfo
 
 # to launch a shell session inside a container
--- tests/run_BabelStream.sh
+++ tests/run_BabelStream.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 NY=1024 ; NZ=1024 ; NX=256 ; TBSIZE=256; NUMTIMES=1000
 BABELSTREAM_ROOT=${PWD}/BabelStream
-rm -rf ${BABELSTREAM_ROOT}
-module list 2>&1 | grep -q -w "rocm"
-if [ $? -eq 1 ]; then
-  echo "rocm module is not loaded"
-  echo "loading default rocm module"
-  module load rocm
+
+if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
+   if [ -z "$CXX" ]; then
+      export CXX=`which CC`
+   fi
+   if [ -z "$CC" ]; then
+      export CC=`which cc`
+   fi
+   if [ -z "$FC" ]; then
+      export FC=`which ftn`
+   fi
+   if [ -z "$HIPCC" ]; then
+      export HIPCC=`which hipcc`
+   fi
+else
+   module list 2>&1 | grep -q -w "rocm"
+   if [ $? -eq 1 ]; then
+     echo "rocm module is not loaded"
+     echo "loading default rocm module"
+     module load rocm
+   fi
+   module load amdflang-new >& /dev/null
+   if [ "$?" == "1" ]; then
+      module load amdclang
+   fi
 fi
+
+rm -rf ${BABELSTREAM_ROOT}
 git clone https://github.com/UoB-HPC/BabelStream.git ${BABELSTREAM_ROOT}
 cd ${BABELSTREAM_ROOT}
 # -DDEFAULT -- good performance
