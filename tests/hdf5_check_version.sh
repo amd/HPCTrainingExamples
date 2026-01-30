a25fd59664524933c7142e2deed010a9defce4f5
Bob Robey <Bob.Robey@amd.com>
2026-01-29 20:56:09 -0600

    Trying new module loads

--- tests/hdf5_check_version.sh
+++ tests/hdf5_check_version.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-module list 2>&1 | grep -q -w "rocm"
-if [ $? -eq 1 ]; then
-  echo "rocm module is not loaded"
-  echo "loading default rocm module"
-  module load rocm
+if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
+   if [ -z "$CXX" ]; then
+      export CXX=`which CC`
+   fi
+   if [ -z "$CC" ]; then
+      export CC=`which cc`
+   fi
+   if [ -z "$FC" ]; then
+      export FC=`which ftn`
+   fi
+else
+   module list 2>&1 | grep -q -w "rocm"
+   if [ $? -eq 1 ]; then
+     echo "rocm module is not loaded"
+     echo "loading default rocm module"
+     module load rocm
+   fi
+   module load amdflang-new >& /dev/null
+   if [ "$?" == "1" ]; then
+      module load amdclang
+   fi
 fi
+
 if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
    module load cray-hdf5-parallel
 else
