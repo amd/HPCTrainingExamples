df14768535cb3ef63516853c4f77ff56e7d6003f
Bob Robey <Bob.Robey@amd.com>
2026-01-30 03:25:42 -0600

    More fixes for HPE testing

--- tests/nodememmodel_openmp_atomics2_xnack_on.sh
+++ tests/nodememmodel_openmp_atomics2_xnack_on.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-module list 2>&1 | grep -q -w "rocm"
-if [ $? -eq 1 ]; then
-  echo "rocm module is not loaded"
-  echo "loading default rocm module"
-  module load rocm
+if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
+   if [ -z "$CXX" ]; then
+      export CXX=`which CC`
+   fi
+   if [ -z "$CC" ]; then
+      export CC=`which cc`
+   fi
+   if [ -z "$FC" ]; then
+      export FC=`which ftn`
+   fi
+   if [ -z "$HIPCC" ]; then
+      export HIPCC=`which hipcc`
+   fi
+else
+   module list 2>&1 | grep -q -w "rocm"
+   if [ $? -eq 1 ]; then
+     echo "rocm module is not loaded"
+     echo "loading default rocm module"
+     module load rocm
+   fi
+   module load amdflang-new >& /dev/null
+   if [ "$?" == "1" ]; then
+      module load amdclang
+   fi
 fi
+
 XNACK_COUNT=`rocminfo | grep xnack | wc -l`
 if [ ${XNACK_COUNT} -lt 1 ]; then
    echo "Skip"
 else
-   if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
-      export CXX=${ROCM_PATH}/llvm/bin/amdclang++
-      export CC=${ROCM_PATH}/llvm/bin/amdclang
-      export FC=${ROCM_PATH}/llvm/bin/amdflang
-   else
-      module load amdclang
-   fi
 
    REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
    cd ${REPO_DIR}/atomics_openmp
--- tests/nodememmodel_openmp_atomics3_xnack_off.sh
+++ tests/nodememmodel_openmp_atomics3_xnack_off.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-module list 2>&1 | grep -q -w "rocm"
-if [ $? -eq 1 ]; then
-  echo "rocm module is not loaded"
-  echo "loading default rocm module"
-  module load rocm
-fi
 if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
-   export CXX=${ROCM_PATH}/llvm/bin/amdclang++
-   export CC=${ROCM_PATH}/llvm/bin/amdclang
-   export FC=${ROCM_PATH}/llvm/bin/amdflang
+   if [ -z "$CXX" ]; then
+      export CXX=`which CC`
+   fi
+   if [ -z "$CC" ]; then
+      export CC=`which cc`
+   fi
+   if [ -z "$FC" ]; then
+      export FC=`which ftn`
+   fi
+   if [ -z "$HIPCC" ]; then
+      export HIPCC=`which hipcc`
+   fi
 else
-   module load amdclang
+   module list 2>&1 | grep -q -w "rocm"
+   if [ $? -eq 1 ]; then
+     echo "rocm module is not loaded"
+     echo "loading default rocm module"
+     module load rocm
+   fi
+   module load amdflang-new >& /dev/null
+   if [ "$?" == "1" ]; then
+      module load amdclang
+   fi
 fi
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
