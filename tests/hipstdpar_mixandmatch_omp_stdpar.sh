08146fc777dd836d793f2bf2644bf40739e9a1e3
Bob Robey <Bob.Robey@amd.com>
2026-01-29 19:54:46 -0600

    Trying new module loads

--- tests/hipstdpar_mixandmatch_omp_stdpar.sh
+++ tests/hipstdpar_mixandmatch_omp_stdpar.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-export HSA_XNACK=1
-module list 2>&1 | grep -q -w "rocm"
-if [ $? -eq 1 ]; then
-  echo "rocm module is not loaded"
-  echo "loading default rocm module"
-  module load rocm
-fi
 if [[ "`printenv |grep -w CRAY |wc -l`" -gt 1 ]]; then
-   export CXX=`which CC`
+   if [ -z "$CXX" ]; then
+      export CXX=`which CC`
+   fi
+   if [ -z "$CC" ]; then
+      export CC=`which cc`
+   fi
+   if [ -z "$FC" ]; then
+      export FC=`which ftn`
+   fi
 else
-   module load amdclang
+   module list 2>&1 | grep -q -w "rocm"
+   if [ $? -eq 1 ]; then
+     echo "rocm module is not loaded"
+     echo "loading default rocm module"
+     module load rocm
+   fi
+   module load amdflang-new >& /dev/null
+   if [ "$?" == "1" ]; then
+      module load amdclang
+   fi
 fi
+
+export HSA_XNACK=1
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 pushd ${REPO_DIR}/HIPStdPar/CXX/MixAndMatch/omp_stdpar
