75d959a02ea772f9bd09fd22c2a580eaba813d9e
Bob Robey <Bob.Robey@amd.com>
2026-03-03 16:41:56 -0600

    Three more julia tests that need the same fix

--- tests/julia_gpu_vec_add.sh
+++ tests/julia_gpu_vec_add.sh
@@ -7,7 +7,7 @@
 # https://github.com/amd/HPCTrainingDock/blob/main/extras/scripts/julia_setup.sh
 
 export CUR_DIR=$PWD
-curl -fsSL https://install.julialang.org | sh -s -- --yes --add-to-path=no -p=${CUR_DIR}/juliaup_install
+curl -fsSL https://install.julialang.org | sh -s -- --yes --add-to-path=no -p=${CUR_DIR}/juliaup_install || true
 export PATH=$PATH:"${CUR_DIR}/juliaup_install/bin"
 juliaup add 1.12
 juliaup default 1.12
