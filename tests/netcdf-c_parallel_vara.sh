917da75da9071d4947302804cd13c2be9807c375
Bob Robey <Bob.Robey@amd.com>
2026-03-03 18:18:10 -0600

    Fixing race condition in parallel testing of netcdf tests

--- tests/netcdf-c_parallel_vara.sh
+++ tests/netcdf-c_parallel_vara.sh
@@ -42,15 +42,15 @@ fi
 # use the compiler used to build netcdf-c
 CC=`nc-config --cc`
 
-rm -rf netcdf-c_test
-mkdir netcdf-c_test
-cd netcdf-c_test
+rm -rf netcdf-c_parallel_vara_test
+mkdir netcdf-c_parallel_vara_test
+cd netcdf-c_parallel_vara_test
 git clone https://github.com/Unidata/netcdf-c.git
 $CC -O2 ./netcdf-c/examples/C/parallel_vara.c -o parallel_vara -L${NETCDF_C_ROOT}/lib -lnetcdf -L${PNETCDF_ROOT}/lib -lpnetcdf
 mpirun -n 4 ./parallel_vara testfile.nc
 ncdump testfile.nc
 cd ..
-rm -rf netcdf-c_test
+rm -rf netcdf-c_parallel_vara_test
 
 
 
