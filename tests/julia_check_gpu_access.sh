06f797c35bfcd47893dfe64a06fc0d840e424464
gcapodagAMD <Giacomo.Capodaglio@amd.com>
2026-03-05 13:47:45 -0600

    Fixing Julia tests

--- tests/julia_check_gpu_access.sh
+++ tests/julia_check_gpu_access.sh
@@ -7,9 +7,9 @@
 # https://github.com/amd/HPCTrainingDock/blob/main/extras/scripts/julia_setup.sh
 
 export CUR_DIR=$(mktemp -d)
-export HOME="${CUR_DIR}"
+export TMPDIR="${CUR_DIR}"
 export JULIA_DEPOT_PATH="${CUR_DIR}/julia_depot"
-curl -fsSL https://install.julialang.org | sh -s -- --yes --add-to-path=no -p=${CUR_DIR}/juliaup_install || true
+curl -fsSL https://install.julialang.org | sh -s -- --yes --add-to-path=no -p=${CUR_DIR}/juliaup_install
 export PATH=$PATH:"${CUR_DIR}/juliaup_install/bin"
 juliaup add 1.12
 juliaup default 1.12
