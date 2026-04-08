9940fdc8471cc8068c0552fcd0dab7391cc71f8f
Johanna Potyka <johanna.potyka@amd.com>
2026-04-08 03:31:13 -0700

    unify copyright text on OpenMP CXX examples

--- Pragma_Examples/OpenMP/CXX/6_device_routines/3_virtual_methods/2_virtual_methods_map/HotScience.hh
+++ Pragma_Examples/OpenMP/CXX/6_device_routines/3_virtual_methods/2_virtual_methods_map/HotScience.hh
@@ -1,3 +1,9 @@
+/*
+Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
+This software is distributed under the MIT License
+
+*/
+
 #include "Science.hh"
 
 class HotScience : public Science
