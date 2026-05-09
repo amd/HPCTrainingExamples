c25b7505b6e54484dd2ce814e68ba3c6b0f47f05
Bob Robey <Bob.Robey@amd.com>
2026-05-09 10:00:48 -0500

    Fixing race condition in OpenMP Fortran make build example

--- tests/openmp_Fortran_build_make.sh
+++ tests/openmp_Fortran_build_make.sh
@@ -31,6 +31,13 @@ else
 
    REPO_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
    cd ${REPO_DIR}/Pragma_Examples/OpenMP/Fortran/BuildExamples
+
+   SRC_DIR=$(pwd)
+   BUILD_DIR=$(mktemp -d)
+   trap "rm -rf ${BUILD_DIR}" EXIT
+   cp * ${BUILD_DIR}
+
+   cd ${BUILDDIR}
    FC=`which amdflang` make openmp_code
    ./openmp_code
 
