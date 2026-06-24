f972183dea23da6b56d4d7ce0dd7c2a02f0b0324
Bob Robey <Bob.Robey@amd.com>
2026-06-24 17:26:28 -0500

    Skipping check when MPICH

--- tests/openmpi_check_rocm_support.sh
+++ tests/openmpi_check_rocm_support.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-module -t list 2>&1 | grep -q "^rocm"
-if [ $? -eq 1 ]; then
-  echo "rocm module is not loaded"
-  echo "loading default rocm module"
-  module load rocm
-fi
-module load openmpi
+if [ -n "${CRAY_MPICH_VERSION:-}" ]; then
+   echo "Skipped - this is MPICH"
+else
+   module -t list 2>&1 | grep -q "^rocm"
+   if [ $? -eq 1 ]; then
+     echo "rocm module is not loaded"
+     echo "loading default rocm module"
+     module load rocm
+   fi
+   module load openmpi
 
-ompi_info | grep "MPI extensions"
+   ompi_info | grep "MPI extensions"
+fi
