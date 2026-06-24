2fb80def6eb7ee4806ee9caa3273bcb6242d384a
Bob Robey <Bob.Robey@amd.com>
2026-06-24 09:49:22 -0500

    Fixing some more tests for Cray systems

--- tests/mpi4py_check_import.sh
+++ tests/mpi4py_check_import.sh
@@ -3,14 +3,30 @@
 # This test imports the mpi4py package in Python to test
 # if Python MPI  is installed and accessible
 
-module -t list 2>&1 | grep -q "^rocm"
-if [ $? -eq 1 ]; then
-  echo "rocm module is not loaded"
-  echo "loading default rocm module"
-  module load rocm
-fi
 if [[ -n "$CRAYPE_VERSION" || -f /etc/cray-release ]]; then
-   module load mpi4py/cray-mpich-${CRAY_MPICH_VERSION}
+   if [ -z "$CXX" ]; then
+      export CXX=`which CC`
+   fi
+   if [ -z "$CC" ]; then
+      export CC=`which cc`
+   fi
+   if [ -z "$FC" ]; then
+      export FC=`which ftn`
+   fi
+else
+   module -t list 2>&1 | grep -q "^rocm"
+   if [ $? -eq 1 ]; then
+     echo "rocm module is not loaded"
+     echo "loading default rocm module"
+     module load rocm
+   fi
+fi
+
+if [[ -n "$CRAYPE_VERSION" || -f /etc/cray-release ]]; then
+   module load mpi4py/cray-mpich-${CRAY_MPICH_VERSION} >& /dev/null
+   if [ "$?" == "1" ]; then
+      module load mpi4py
+   fi
 else
    module load mpi4py
 fi
