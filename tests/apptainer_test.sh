3a6f57bc5b10dcb37204e4d5133b7624963d1d5a
Bob Robey <Bob.Robey@amd.com>
2026-07-29 21:53:16 -0500

    Fixing apptainer host/container conflict

--- tests/apptainer_test.sh
+++ tests/apptainer_test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-module -t list 2>&1 | grep -q "^rocm"
-if [ $? -eq 1 ]; then
-  echo "rocm module is not loaded"
-  echo "loading default rocm module"
-  module load rocm
-fi
-apptainer exec --rocm docker://rocm/dev-ubuntu-24.04:7.2.4 rocminfo
+#module -t list 2>&1 | grep -q "^rocm"
+#if [ $? -eq 1 ]; then
+#  echo "rocm module is not loaded"
+#  echo "loading default rocm module"
+#  module load rocm
+#fi
+# No --rocm necessary -- container has rocm included
+# if want to use host rocm, load the rocm module and use a container without rocm
+apptainer exec docker://rocm/dev-ubuntu-24.04:7.2.4 rocminfo
 
 # to launch a shell session inside a container
-# apptainer shell --rocm  docker://rocm/dev-ubuntu-24.04:7.2.4
+# apptainer shell docker://rocm/dev-ubuntu-24.04:7.2.4
