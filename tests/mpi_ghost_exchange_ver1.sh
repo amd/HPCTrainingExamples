d5b13b06d739c0b9756223eb4aefb4560a1d0ea3
Daniele Bagni <danieleb@xilinx.com>
2026-09-08 09:46:45 +0200

    Use ROCm clang for Ghost unified-memory examples
    Signed-off-by: Daniele Bagni <danieleb@xilinx.com>

--- tests/mpi_ghost_exchange_ver1.sh
+++ tests/mpi_ghost_exchange_ver1.sh
@@ -36,6 +36,11 @@ BUILD_DIR=$(mktemp -d)
 trap "rm -rf ${BUILD_DIR}" EXIT
 cd ${BUILD_DIR}
 
+if [ -n "${ROCM_PATH:-}" ] && [ -x "${ROCM_PATH}/bin/amdclang++" ] && [ -x "${ROCM_PATH}/bin/amdclang" ]; then
+   export CXX="${ROCM_PATH}/bin/amdclang++"
+   export CC="${ROCM_PATH}/bin/amdclang"
+fi
+
 cmake ${SRC_DIR}
 make
 
